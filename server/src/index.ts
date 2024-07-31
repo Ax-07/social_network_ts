ee325adb088fb662694d32ebb96280d0cdd1015b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-07-31 23:36:23 +0200

    ajout de la route pour la gestion des users

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,6 +1,8 @@
-import express, { Request, Response } from 'express';
-import cors from 'cors';
-import * as dotenv from 'dotenv';
+import express, { Request, Response } from "express";
+import cors from "cors";
+import * as dotenv from "dotenv";
+import userRoutes from "./routes/user.routes";
+import db from "./models";
 
 dotenv.config();
 const app = express();
@@ -9,12 +11,17 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get('/', (req: Request, res: Response) => {
-  res.send('Hello World!');
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello World!");
 });
 
+app.use("/api", userRoutes);
+
+// Synchronisation de la base de données et lancement du serveur
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
+db.sequelize.sync().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+  });
 });
