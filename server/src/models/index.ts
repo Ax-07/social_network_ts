34dd1bac9e93b0a6d6839fd18f55f5a60c563347
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-01 22:52:11 +0200

    ajout des model post et comment et ajout des relations

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -1,5 +1,7 @@
 import { Sequelize } from "sequelize";
 import { initializeUserModel, User } from "./user.model";
+import { initializePostModel, Post } from "./post.model";
+import { initializeCommentModel, Comment } from "./comment.model";
 
 const sequelize = new Sequelize({
   dialect: 'sqlite',
@@ -10,13 +12,27 @@ interface Database {
   Sequelize: typeof Sequelize;
   sequelize: Sequelize;
   User: typeof User;
+  Post: typeof Post;
+  Comment: typeof Comment;
 }
 
 const db: Database = {
   Sequelize,
   sequelize,
   User: initializeUserModel(sequelize),
+  Post: initializePostModel(sequelize),
+  Comment: initializeCommentModel(sequelize),
 };
+
+// Définir les associations après l'initialisation des modèles
+db.User.hasMany(db.Post, { foreignKey: 'userId', as: 'posts' });
+db.Post.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+
+db.User.hasMany(db.Comment, { foreignKey: 'userId', as: 'comments' });
+db.Comment.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+
+db.Post.hasMany(db.Comment, { foreignKey: 'postId', as: 'comments' });
+db.Comment.belongsTo(db.Post, { foreignKey: 'postId', as: 'post' });
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
