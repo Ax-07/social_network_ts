5cb4edced8229b87f11e43f95c266366649ec13c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-04 16:18:37 +0200

    ajout des routes pour ajouter les images de profile et de couverture

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
-import { createUser, deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/user.controller";
+import { createUser, deleteUser, getAllUsers, getUserById, updatePictureProfile, updateCoverPicture, updateUser } from "../controllers/user.controller";
+import uploadFileMiddleware from "../middleware/multer.middleware";
 
 const router = Router();
 
@@ -7,6 +8,8 @@ router.post('/users', createUser);
 router.get('/users', getAllUsers);
 router.get('/users/:id', getUserById);
 router.patch('/users/:id', updateUser);
+router.patch('/users/:id/profile-picture',uploadFileMiddleware, updatePictureProfile);
+router.patch('/users/:id/cover-picture',uploadFileMiddleware, updateCoverPicture);
 router.delete('/users/:id', deleteUser);
 
 export default router;
