d1792cc029ecb4e1b5412cf0d064388829c852b2
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-04 17:13:26 +0200

    creation fonction pour valider les entrées des posts

---
+++ server/src/utils/functions/validatePostEntry.ts
@@ -0,0 +1,40 @@
+import { Optional } from "sequelize";
+import { PostAttributes } from "../../models/post.model";
+
+interface PostEntry extends Optional<PostAttributes, "id"> {};
+
+const validatePostEntry = (entry: PostEntry) => {
+    const errors: string[] = [];
+
+    if (entry.userId && (typeof entry.userId !== 'string' || entry.userId.length === 0)) {
+        errors.push('UserId must be a non-empty string');
+    }
+
+    if (entry.title && (typeof entry.title !== 'string' || entry.title.length === 0)) {
+        errors.push('Title must be a non-empty string');
+    }
+
+    if (entry.content && (typeof entry.content !== 'string' || entry.content.length === 0)) {
+        errors.push('Content must be a non-empty string');
+    }
+
+    if (entry.picture && (typeof entry.picture !== 'string' || !/^https?:\/\/\S+\.\S+$/.test(entry.picture))) {
+        errors.push('Picture must be a string');
+    }
+
+    if (entry.video && (typeof entry.video !== 'string' || !/^https?:\/\/\S+\.\S+$/.test(entry.video))) {
+        errors.push('Video must be a string');
+    }
+
+    if (entry.likers !== undefined && !Array.isArray(entry.likers)) {
+        errors.push('Likers must be an array of strings');
+    }
+
+    if (entry.dislikers !== undefined && !Array.isArray(entry.dislikers)) {
+        errors.push('Dislikers must be an array of strings');
+    }
+
+    return errors;
+}
+
+export default validatePostEntry;
