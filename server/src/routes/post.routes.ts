c22b75a1c0ff95cb821dcebbe46614f553963e88
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-04 17:14:45 +0200

    ajout du middleware pour la gestion des images et des videos

--- server/src/routes/post.routes.ts
+++ server/src/routes/post.routes.ts
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { createPost, deletePost, getAllPosts, getPostById, updatePost } from "../controllers/post.controller";
+import uploadFileMiddleware from "../middleware/multer.middleware";
 
 const router = Router();
 
-router.post('/posts', createPost);
+router.post('/posts',uploadFileMiddleware, createPost);
 router.get('/posts', getAllPosts);
 router.get('/posts/:id', getPostById);
-router.patch('/posts/:id', updatePost);
+router.patch('/posts/:id',uploadFileMiddleware, updatePost);
 router.delete('/posts/:id', deletePost);
 
 export default router;
