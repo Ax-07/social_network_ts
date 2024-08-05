db4449d123c13fd95a3541d4b22deae551d414cb
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-05 18:16:49 +0200

    ajout du middleware multer pour l'ajout de media dans les commentaires

--- server/src/routes/comment.routes.ts
+++ server/src/routes/comment.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { createComment, deleteComment, getAllComments, getCommentById, updateComment } from "../controllers/comment.controller";
+import uploadFileMiddleware from "../middleware/multer.middleware";
 
 const router = Router();
 
-router.post('/comments', createComment);
+router.post('/comments', uploadFileMiddleware, createComment);
 router.get('/comments', getAllComments);
 router.get('/comments/:id', getCommentById);
 router.patch('/comments/:id', updateComment);
