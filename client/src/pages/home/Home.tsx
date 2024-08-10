4804276172347a0e9d69b1c9287c926d97ed4430
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-10 23:20:33 +0200

    maj des routes posts

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,20 +1,20 @@
 import TabList from "../../components/tabList/TabList";
 import { Routes, Route } from "react-router-dom";
-import FeedList from "../../layouts/feed/FeedList";
 import AddPost from "../../components/post/AddPost";
+import PostList from "../../components/post/PostsList";
 
 const Home = () => {
   return (
     <div className="home">
       <TabList
         links={[
-          { name: "Pour vous", to: "/home/feed" },
+          { name: "Pour vous", to: "/home/posts" },
           { name: "Abonnements", to: "/home/abonnements" },
         ]}
       />
       <AddPost />
       <Routes>
-        <Route path="/feed" element={<FeedList />} />
+        <Route path="/posts" element={<PostList />} />
         <Route path="/abonnements" element={<h2>Abonnements</h2>} />
         <Route path="/coding-lab" element={<h2>CodingLab</h2>} />
       </Routes>
