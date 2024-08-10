4344e475990c6cefff0b09638b5f7feb81bf152f
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-10 22:38:39 +0200

    ajout responsive et correction chemin d'acces de l'image de fond

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -9,18 +9,21 @@ import BooKmarks from "./pages/bookmarks/BooKmarks";
 import Lists from "./pages/lists/Lists";
 import Profile from "./pages/profile/Profile";
 import More from "./pages/more/More";
+import { useWindowSize } from "./utils/hooks/useWindowSize";
 
 function App() {
+  const { windowWidth } = useWindowSize();
+  const isTablet = windowWidth <= 1020;
   return (
     <div className="app">
       <picture className="picture-background">
-        <source srcSet={"/src/assets/images/app-background/mobile-unsplash_eNoeWZkO7Zc.webp"} media="(max-width: 600px)" />
+        <source srcSet={"src/assets/images/app-background/mobile-unsplash_eNoeWZkO7Zc.webp"} media="(max-width: 600px)" />
         <source
           srcSet="src/assets/images/app-background/tablet-unsplash_eNoeWZkO7Zc.webp"
           media="(min-width: 601px) and (max-width: 1024px)"
         />
-        <source srcSet="/src/assets/images/app-background/desktop-unsplash_eNoeWZkO7Zc.webp" media="(min-width: 1025px)" />
-        <img src="/src/assets/images/app-background/mobile-image.webp" alt="image de fond de l'application" loading="lazy" />
+        <source srcSet="src/assets/images/app-background/desktop-unsplash_eNoeWZkO7Zc.webp" media="(min-width: 1025px)" />
+        <img src="src/assets/images/app-background/mobile-image.webp" alt="image de fond de l'application" loading="lazy" />
       </picture>
       <SideMenu />
       <main>
@@ -34,8 +37,8 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/more" element={<More />} />
         </Routes>
+      {!isTablet && <SideColumn />}
       </main>
-      <SideColumn />
     </div>
   );
 }
