43ff30a1ac143b00c23a617616e5f7977e18a376
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-12 21:26:25 +0200

    ajout de la route repost

--- server/src/routes/post.routes.ts
+++ server/src/routes/post.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { createPost, deletePost, getAllPosts, getPostById, updatePost } from "../controllers/post.controller";
+import { createPost, rePost, deletePost, getAllPosts, getPostById, updatePost } from "../controllers/post.controller";
 import uploadFileMiddleware from "../middleware/multer.middleware";
 
 const router = Router();
 
 router.post('/posts',uploadFileMiddleware, createPost);
+router.post('/reposts',uploadFileMiddleware, rePost);
 router.get('/posts', getAllPosts);
 router.get('/posts/:id', getPostById);
 router.patch('/posts/:id',uploadFileMiddleware, updatePost);
