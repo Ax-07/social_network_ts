0b828f2f5ea92d442478199dbd39708399f0392e
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-12 21:23:20 +0200

    maj controller comment pour ajout un post dans les commentaires

--- server/src/controllers/comment.controller.ts
+++ server/src/controllers/comment.controller.ts
@@ -3,21 +3,52 @@ import db from '../models';
 import { handleControllerError } from '../utils/errors/controllers.error';
 import validateCommentEntry from '../utils/functions/validateCommentEntry';
 
+
 const createComment = async (req: Request, res: Response) => {
-  const { postId, userId, content } = req.body;
-  const picture = res.locals.filePath; // Utilisez le chemin enregistré dans res.locals
-  const video = res.locals.filePath; // Utilisez le chemin enregistré dans res.locals
-  if (!postId || !userId || !content) {
+  const { postId, userId, content, commentedPostId } = req.body;
+  const media = res.locals.filePath; // Utilisez le chemin enregistré dans res.locals
+
+  if (!postId || !userId || (!content && !media && !commentedPostId)) {
     return res.status(400).json({ message: 'Missing required information' });
   }
-  const errors = validateCommentEntry({ postId, userId, content, picture, video });
+
+  const errors = validateCommentEntry({ postId, userId, content, media, commentedPostId });
   if (errors.length > 0) {
     return res.status(400).json({ message: 'Validation error', errors });
   }
+
+  // Commencer une transaction
+  const transaction = await db.sequelize.transaction();
+
   try {
-    const comment = await db.Comment.create({ postId, userId, content, picture, video });
+    // Créer le commentaire
+    const comment = await db.Comment.create(
+      { postId, userId, content, media, commentedPostId },
+      { transaction }
+    );
+
+    // Mettre à jour le post partagé en ajoutant l'utilisateur aux reposters, s'il y a un commentedPostId
+    if (commentedPostId) {
+      const commentedPost = await db.Post.findByPk(commentedPostId, { transaction });
+      if (!commentedPost) {
+        await transaction.rollback();
+        return res.status(404).json({ message: `Commented post ${commentedPostId} not found` });
+      }
+
+      const reposters = commentedPost.reposters || [];
+      if (!reposters.includes(userId)) {
+        reposters.push(userId);
+        await commentedPost.update({ reposters }, { transaction });
+      }
+    }
+
+    // Valider la transaction
+    await transaction.commit();
+
     res.status(201).json({ message: 'Comment created successfully', comment });
   } catch (error) {
+    // Annuler la transaction en cas d'erreur
+    await transaction.rollback();
     handleControllerError(res, error, 'An error occurred while creating the comment.');
   }
 };
