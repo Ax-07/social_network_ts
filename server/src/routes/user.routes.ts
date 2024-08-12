bd9228eac36584d822a01d83e80ef8b4eca10382
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-12 21:26:46 +0200

    ajout de la route bookmarks

--- server/src/routes/user.routes.ts
+++ server/src/routes/user.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { createUser, deleteUser, getAllUsers, getUserById, updatePictureProfile, updateCoverPicture, updateUser } from "../controllers/user.controller";
+import { addToBookmarks } from "../controllers/bookmarks.controllers";
 import uploadFileMiddleware from "../middleware/multer.middleware";
 
 const router = Router();
@@ -11,5 +12,7 @@ router.patch('/users/:id', updateUser);
 router.patch('/users/:id/profile-picture',uploadFileMiddleware, updatePictureProfile);
 router.patch('/users/:id/cover-picture',uploadFileMiddleware, updateCoverPicture);
 router.delete('/users/:id', deleteUser);
+
+router.patch('/users/:id/bookmarks', addToBookmarks);
 
 export default router;
