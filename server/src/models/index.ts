693af409a1252db567009eaba6c463ed54ee5405
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-12 21:26:08 +0200

    maj des relations entre les tables

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -25,6 +25,8 @@ const db: Database = {
 };
 
 // Définir les associations après l'initialisation des modèles
+// foreignKey est utilisé pour spécifier le nom de la clé étrangère dans la base de données
+// as est utilisé pour spécifier le nom de l'association dans les requêtes 
 db.User.hasMany(db.Post, { foreignKey: 'userId', as: 'posts' });
 db.Post.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
 
@@ -33,6 +35,12 @@ db.Comment.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
 
 db.Post.hasMany(db.Comment, { foreignKey: 'postId', as: 'comments' });
 db.Comment.belongsTo(db.Post, { foreignKey: 'postId', as: 'post' });
+
+db.Post.hasMany(db.Post, { foreignKey: 'originalPostId', as: 'reposts' });
+db.Post.belongsTo(db.Post, { foreignKey: 'originalPostId', as: 'originalPost' });
+
+db.Post.hasMany(db.Comment, { foreignKey: 'commentedPostId', as: 'commentedPosts' });
+db.Comment.belongsTo(db.Post, { foreignKey: 'commentedPostId', as: 'commentedPost' });
 
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
