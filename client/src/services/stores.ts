54ec3b10742c7dee1f2bca1327ced662ccbdc339
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-13 23:28:19 +0200

    maj du store

--- client/src/services/stores.ts
+++ client/src/services/stores.ts
@@ -4,21 +4,29 @@ import { persistStore, persistReducer } from "redux-persist";
 import storageSession from "redux-persist/lib/storage/session";
 import { postApi } from "./api/postApi";
 import { UserApi } from "./api/userApi";
+import { authApi } from "./auth/authApi";
+import { googleAuthApi } from "./auth/googleAuthApi";
+import authSlice from "./auth/authSlice";
 
 const persistConfig = {
     key: "root",
     storage: storageSession,
-};
+}
+
+const persistedAuthSlice = persistReducer(persistConfig, authSlice);
 
 const store = configureStore({
     reducer: {
         [postApi.reducerPath]: postApi.reducer,
         [UserApi.reducerPath]: UserApi.reducer,
+        [authApi.reducerPath]: authApi.reducer,
+        [googleAuthApi.reducerPath]: googleAuthApi.reducer,
+        auth: persistedAuthSlice,
     },
     middleware: (getDefaultMiddleware) =>
         getDefaultMiddleware({
             serializableCheck: false,
-        }).concat(postApi.middleware, UserApi.middleware),
+        }).concat(postApi.middleware, UserApi.middleware, authApi.middleware, googleAuthApi.middleware),
 });
 
 const persistor = persistStore(store);
