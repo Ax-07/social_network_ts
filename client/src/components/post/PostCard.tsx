5108d4aef580650f44bc0d6c3274b74f5008fe94
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-13 23:18:22 +0200

    modif du chemin pour recuperer les images ou les videos

--- client/src/components/post/PostCard.tsx
+++ client/src/components/post/PostCard.tsx
@@ -1,13 +1,14 @@
 import type { FunctionComponent } from 'react';
 import type { Post } from '../../services/api/postApi';
+import BtnLike from '../btn-like/BtnLike';
 
 export type PostProps = {
   post: Post;
 };
 
 const PostCard: FunctionComponent<PostProps> = ({post}) => {
-  const isWebp = post.picture?.endsWith('.webp');
-  const isMp4 = post.picture?.endsWith('.mp4');
+  const isWebp = post.media?.endsWith('.webp');
+  const isMp4 = post.media?.endsWith('.mp4');
 
   return (
     <article className='post__card'>
@@ -15,11 +16,10 @@ const PostCard: FunctionComponent<PostProps> = ({post}) => {
         <img src="" alt="" />
       </div>
       <div className="post__card-wrapper">
-        <h2 className='post__card-title'>{post.title}</h2>
         <p className='post__card-content'>{post.content}</p>
-        {post.picture && isWebp && <img className='post__card-img' src={post.picture} alt={post.title} loading='lazy'/>}
-        {post.picture && isMp4 && <video className='post__card-video' src={post.picture} controls/>}
-        {post.likers && <p className='post__card-like'>{post.likers.length} likers</p>}
+        {post.media && isWebp && <img className='post__card-img' src={post.media} alt={""} loading='lazy'/>}
+        {post.media && isMp4 && <video className='post__card-video' src={post.media} controls/>}
+        <BtnLike post={post}/>
         {post.dislikers && <p className='post__card-dislike'>{post.dislikers.length} dislikers</p>}
       </div>
     </article>
