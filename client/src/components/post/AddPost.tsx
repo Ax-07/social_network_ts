e4195438d87acf8b196c495c9093d75d51f0f4d5
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-13 23:17:38 +0200

    maj du formulaire

--- client/src/components/post/AddPost.tsx
+++ client/src/components/post/AddPost.tsx
@@ -1,16 +1,14 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import { UserPicture } from "../userProfile/UserProfileThumbnail";
 import PreviewPicture from "../addPicture/PreviewPicture";
 import InputPicture from "../addPicture/InputPicure";
 import { useAddPostMutation, useGetPostsQuery } from "../../services/api/postApi";
 import Button from "../button/Button";
-import ButtonModal from "../modal/ButtonModal";
 
-const userId = "7eb24187-6ca9-43da-9357-ff675903cb8d";
+const userId = "4a655857-40a7-4814-baa1-0f05fd46f73b";
 
 interface FormState {
   userId: string;
-  title: string;
   content: string;
   file: File | string;
 }
@@ -23,14 +21,17 @@ interface AddPostProps {
 const AddPost = ({ origin, onClose }: AddPostProps) => {
   const [form, setForm] = useState<FormState>({
     userId: userId,
-    title: "",
     content: "",
     file: "",
   });
 
   const [preview, setPreview] = useState<string>("");
+  const [mimetype, setMimetype] = useState<string>("");
+
+  const inputFileRef = useRef<HTMLInputElement>(null); // Référence pour l'entrée de fichier
+
   const isPreview = form.file !== "";
-  const isValidForm = form.title && form.content;
+  const isValidForm = form.content;
 
   const [addPost, { isError, isLoading }] = useAddPostMutation();
   const { refetch } = useGetPostsQuery();
@@ -38,20 +39,19 @@ const AddPost = ({ origin, onClose }: AddPostProps) => {
   const resetForm = () => {
     setForm({
       userId: userId,
-      title: "",
       content: "",
       file: "",
     });
     setPreview("");
+    setMimetype("");
   };
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     const formData = new FormData();
     formData.append("userId", form.userId);
-    formData.append("title", form.title);
     formData.append("content", form.content);
-    formData.append("picture", form.file);
+    formData.append("media", form.file);
 
     await addPost(formData);
     if (isError) {
@@ -68,6 +68,7 @@ const AddPost = ({ origin, onClose }: AddPostProps) => {
     const reader = new FileReader();
     reader.onloadend = () => {
       setPreview(reader.result as string);
+      setMimetype(image.type);
     };
     reader.readAsDataURL(image);
   };
@@ -75,7 +76,7 @@ const AddPost = ({ origin, onClose }: AddPostProps) => {
   return (
     <div className={`addpost addpost--${origin}`}>
       <div className="addpost__avatar">
-      <UserPicture />
+        <UserPicture />
       </div>
       <form onSubmit={handleSubmit} className="addpost__form">
         <div className="addpost__header">
@@ -84,15 +85,6 @@ const AddPost = ({ origin, onClose }: AddPostProps) => {
             <option value="private">Privé</option>
           </select>
         </div>
-        {/* <input
-          className="addpost__input"
-          type="text"
-          placeholder="Titre"
-          name="title"
-          id="title"
-          onChange={(e) => setForm({ ...form, title: e.target.value })}
-          value={form.title}
-        /> */}
         <textarea
           className="addpost__input"
           placeholder="Quoi de neuf ?"
@@ -103,13 +95,21 @@ const AddPost = ({ origin, onClose }: AddPostProps) => {
           value={form.content}
         />
         {isPreview && (
-          <PreviewPicture image={preview} onCancel={() => {
-            setForm({ ...form, file: "" });
-            setPreview("");
-          }} />
+          <PreviewPicture
+            media={preview}
+            mimetype={mimetype}
+            onCancel={() => {
+              setForm({ ...form, file: "" });
+              setPreview("");
+              setMimetype("");
+              if (inputFileRef.current) {
+                inputFileRef.current.value = ""; // Réinitialiser la valeur de l'entrée de fichier
+              }
+            }}
+          />
         )}
         <div className="addpost__bottom">
-          <InputPicture setImage={handleFileChange}/>
+          <InputPicture setMedia={handleFileChange} inputRef={inputFileRef} />
           <Button type="submit" className="btn__post-submit" disabled={!isValidForm}>
             Publier
           </Button>
