d67c8ad0e4f49e83f4cbc5b628908b9ec9618928
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-13 23:36:40 +0200

    maj de la fonction qui gere les erreur du server

--- server/src/utils/errors/server.error.ts
+++ server/src/utils/errors/server.error.ts
@@ -1,8 +1,10 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 
-const serverError = (err: Error, req: Request, res: Response) => {
-    console.error(err.stack);
+const serverError = (err: Error, req: Request, res: Response, next: NextFunction) => {
+    if (res.headersSent) {
+      return next(err);
+    }
     res.status(500).json({ message: 'Internal server error' });
-};
+  };
 
 export default serverError;
