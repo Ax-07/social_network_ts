6d41fd704b245bce892bdec1daae6ead0e43b325
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-13 23:21:31 +0200

    creation de la page d'authentification

---
+++ client/src/pages/auth/AuthPage.tsx
@@ -0,0 +1,71 @@
+import { useRef, useState } from "react";
+import { useDispatch } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { useLoginMutation } from "../../services/auth/authApi";
+import { useGoogleLoginMutation } from "../../services/auth/googleAuthApi";
+import GoogleAuthBtn from "../../components/google-auth-btn/GoogleAuthBtn";
+import { loginSuccess } from "../../services/auth/authSlice";
+import { User } from "../../services/api/userApi";
+
+interface Error {
+  data: {
+    error: string;
+  };
+}
+
+const AuthPage = () => {
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+  const loginFormData = useRef<HTMLFormElement>(null);
+  const [login] = useLoginMutation();
+  const [googleLogin] = useGoogleLoginMutation();
+  const [error, setError] = useState<string | null>(null);
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setError(null); // Réinitialise l'erreur à chaque tentative de connexion
+
+    try {
+      const response = await login({
+        email: loginFormData.current?.email.value,
+        password: loginFormData.current?.password.value,
+      }).unwrap(); // Utilisation de unwrap pour obtenir la réponse directement ou lancer une erreur
+
+      const data: { user: User; token: string } = response;
+      dispatch(loginSuccess({ user: data.user, token: data.token }));
+    } catch (err) {
+      setError((err as Error).data.error);
+    } finally {
+      navigate("/");
+    }
+  };
+
+  return (
+    <div>
+      <h1>Auth Page</h1>
+      <GoogleAuthBtn />
+      <form ref={loginFormData} onSubmit={handleLogin}>
+        <span className="error" color="red">
+          {error}
+        </span>
+        <div className="input-wrapper">
+          <label htmlFor="email">Username</label>
+          <input type="email" id="email" name="email" />
+        </div>
+        <div className="input-wrapper">
+          <label htmlFor="password">Password</label>
+          <input type="password" id="password" name="password" />
+        </div>
+        <div className="input-remember">
+          <input type="checkbox" id="remember-me" name="remember-me" />
+          <label htmlFor="remember-me">Remember me</label>
+        </div>
+        <button type="submit" className="sign-in-button">
+          Sign In
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default AuthPage;
