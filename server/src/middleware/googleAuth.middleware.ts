c9194424d736fe5500ff0e8e4e6128ac10b9b658
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-13 23:32:26 +0200

    creation du middleware googleAuth

---
+++ server/src/middleware/googleAuth.middleware.ts
@@ -0,0 +1,35 @@
+import { OAuth2Client } from "google-auth-library";
+import { Response, NextFunction } from "express";
+import { CustomRequest } from '../utils/types/customRequest';
+
+
+const client = new OAuth2Client(process.env.GOOGLE_CLIENT_ID);
+
+const verifyGoogleToken = async (token: string) => {
+    try {
+        const ticket = await client.verifyIdToken({
+            idToken: token,
+            audience: process.env.GOOGLE_CLIENT_ID,
+        });
+        const payload = ticket.getPayload();
+        return payload; // Add this line to return the payload
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+const googleAuthMiddleware = async (req: CustomRequest, res: Response, next: NextFunction) => {
+    const token = req.body.token;
+    if (!token) {
+        return res.status(401).json({ message: 'No token provided' });
+    }
+    try {
+        const payload = await verifyGoogleToken(token);
+        req.user = payload as CustomRequest['user'];
+        next();
+    } catch (error) {
+        return res.status(401).json({ message: 'Unauthorized' });
+    }
+}
+
+export { googleAuthMiddleware }
