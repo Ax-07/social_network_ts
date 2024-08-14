282c49dbaa80973bbeaa44e8486525623fac91b4
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-14 21:28:57 +0200

    ajout image de profile par default

--- client/src/components/userProfile/UserProfileThumbnail.tsx
+++ client/src/components/userProfile/UserProfileThumbnail.tsx
@@ -1,10 +1,25 @@
+import { useSelector } from "react-redux";
 import { useWindowSize } from "../../utils/hooks/useWindowSize";
+import { RootState } from "../../services/stores";
+import { NavLink } from "react-router-dom";
 
 export const UserPicture = () => {
-  return  <img className="userprofile__picture" src="https://via.placeholder.com/40" alt="User Profile Thumbnail" loading="lazy"/>;
+  const userPicture = useSelector((state: RootState) => state.auth.user?.profilPicture);
+  return (
+    <NavLink to="/profile">
+    <img 
+      className="userprofile__picture" 
+      src={userPicture ? userPicture : "/images/Default-user-picture.png"} 
+      alt="User Profile Thumbnail" 
+      loading="lazy"
+    />
+    </NavLink>
+  );
 }
 
+
 const UserProfileThumbnail = () => {
+  const user = useSelector((state: RootState) => state.auth.user); console.log(user);
   const { windowWidth } = useWindowSize();
   const isTablet = windowWidth <= 1280;
   return (
@@ -13,8 +28,8 @@ const UserProfileThumbnail = () => {
       {!isTablet && (
         <>
           <div className="userprofile__thumbnail-info">
-            <h3 className="fs-15-700">Username</h3>
-            <p className="fs-15-600">@email</p>
+            <h3 className="fs-15-700">{user?.username}</h3>
+            <p className="fs-15-600">{user?.email}</p>
           </div>
           <span className="fs-16-700">...</span>
         </>
