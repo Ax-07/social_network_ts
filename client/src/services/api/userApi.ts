25afb4c1243b63d3333d0e06503645c74873b0ef
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-15 18:39:23 +0200

    ajout proprietes manquantes et correction de type

--- client/src/services/api/userApi.ts
+++ client/src/services/api/userApi.ts
@@ -1,16 +1,20 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export interface User {
-    id: number; // Identifiant unique de l'utilisateur
+    id: string; // Identifiant unique de l'utilisateur
     googleId?: string; // Identifiant Google pour l'authentification OAuth
     username: string; // Nom d'utilisateur
+    handle?: string; // nom d'utilisateur unique
     email?: string; // Adresse e-mail
     password: string; // Mot de passe haché
     profilPicture?: string; // Image de profil
     coverPicture?: string; // Image de couverture
     bio?: string; // Biographie
+    birthdate?: Date; // Date de naissance
     followers?: string[]; // Liste des abonnés
     followings?: string[]; // Liste des abonnements
+    bookmarks?: string[]; // Liste des signets
+    createdAt?: Date; // Date de création
 }
 
 const localUrl = "http://localhost:8080/api";
@@ -22,7 +26,7 @@ export const UserApi = createApi({
         getUsers: builder.query<User[], void>({
             query: () => "/users",
         }),
-        getUserById: builder.query<User, number>({
+        getUserById: builder.query<User, string>({
             query: (id) => `/users/${id}`,
         }),
         createUser: builder.mutation<User, Partial<User>>({
