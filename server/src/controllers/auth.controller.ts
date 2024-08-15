6c361805cb8c4bca026f665b779a3357123bc1a2
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-15 18:42:03 +0200

    ajout de la creation dans nom unique (handle) lors de la creation d'un utilisateur

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,18 +3,26 @@ import jwt from 'jsonwebtoken';
 import db from '../models';
 import { handleControllerError } from '../utils/errors/controllers.error';
 import validateUserEntry from '../utils/functions/validateUserEntry';
+import generateUniqueHandle from '../utils/functions/generateUniqueHandle';
 
 const { User } = db;
 
 const signUp = async (req: Request, res: Response) => {
-    const { email, password, username } = req.body;
+    const { email, password, username, handle } = req.body;
     const errors = validateUserEntry(req.body);
     if (errors.length > 0) {
         return res.status(400).json({ message: 'Validation error', errors });
     }
+    if (handle) {
+        const existingHandle = await User.findOne({ where: { handle } });
+        if (existingHandle) {
+          return res.status(400).json({ error: "Handle already taken" });
+        }
+      }
+      const userHandle = handle || await generateUniqueHandle(username);
 
     try {
-        const user = await User.create({ email, password, username });
+        const user = await User.create({ email, password, username, handle: userHandle });
         return res.status(201).json({ message: 'User created successfully', user });
     } catch (error) {
         handleControllerError(res, error, 'An error occurred while creating the user.');
@@ -39,7 +47,7 @@ const signIn = async (req: Request, res: Response) => {
             return res.status(401).json({ message: 'Invalid password' });
         }
 
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '24h' });
+        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '24h' }); console.log(token);
         return res.status(200).json({ message: 'User signed in successfully', data: { user, token } });
     } catch (error) {
             console.error(error);
