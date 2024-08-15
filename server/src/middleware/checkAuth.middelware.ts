16f8cf68f795a2598a51bee44ea2dbc4a54736bf
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-15 18:44:20 +0200

    correction de la propriete id

--- server/src/middleware/checkAuth.middelware.ts
+++ server/src/middleware/checkAuth.middelware.ts
@@ -3,13 +3,13 @@ import jwt, { JwtPayload } from 'jsonwebtoken';
 import { CustomRequest } from "../utils/types/customRequest";
 
 const checkAuth = async (req: CustomRequest, res: Response, next: NextFunction) => {
-    const token = req.headers.authorization?.split(' ')[1];
+    const token = req.headers.authorization?.split(' ')[1]; console.log("token: ", token);
     if (!token) {
         return res.status(401).json({ message: 'No token provided' });
     }
     try {
-        const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;
-        const userId = decodedToken.userId;
+        const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;  console.log("decodedToken: ", decodedToken);
+        const userId = decodedToken.id; console.log("userId: ", userId);
         req.auth = userId;
         next();
     } catch (error) {
