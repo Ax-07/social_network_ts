c4cc4f36d2cc13f3fe727ef583882f726f207aa6
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-15 18:27:34 +0200

    ajout d'une modale menu et du logout

--- client/src/components/userProfile/UserProfileThumbnail.tsx
+++ client/src/components/userProfile/UserProfileThumbnail.tsx
@@ -1,40 +1,72 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useWindowSize } from "../../utils/hooks/useWindowSize";
 import { RootState } from "../../services/stores";
 import { NavLink } from "react-router-dom";
+import { logout } from "../../services/auth/authSlice";
+import { useState } from "react";
 
 export const UserPicture = () => {
-  const userPicture = useSelector((state: RootState) => state.auth.user?.profilPicture);
+  const user = useSelector((state: RootState) => state.auth?.user);
   return (
-    <NavLink to="/profile">
-    <img 
-      className="userprofile__picture" 
-      src={userPicture ? userPicture : "/images/Default-user-picture.png"} 
-      alt="User Profile Thumbnail" 
-      loading="lazy"
-    />
+    <NavLink to={`/profile/${user?.id}`}>
+      <img
+        className="userprofile__picture"
+        src={
+          user?.profilPicture
+            ? user.profilPicture
+            : "/images/Default-user-picture.png"
+        }
+        alt="User Profile Thumbnail"
+        loading="lazy"
+      />
     </NavLink>
   );
-}
-
+};
 
 const UserProfileThumbnail = () => {
-  const user = useSelector((state: RootState) => state.auth.user); console.log(user);
+  const [isOpen, setIsOpen] = useState(false);
+  const dispatch = useDispatch();
+  const user = useSelector((state: RootState) => state.auth.user);
   const { windowWidth } = useWindowSize();
   const isTablet = windowWidth <= 1280;
+  const handleLogout = () => {
+    dispatch(logout());
+    setIsOpen(false);
+  };
   return (
-    <div className="userprofile__thumbnail">
-      <UserPicture />
-      {!isTablet && (
+    <>
+      <div className="userprofile__thumbnail" onClick={() => setIsOpen(!isOpen)}>
+        <img className="userprofile__picture"
+          src={ user?.profilPicture ? user.profilPicture : "/images/Default-user-picture.png" }
+          alt="User Profile Thumbnail"
+          loading="lazy"
+        />
+        {!isTablet && (
+          <>
+            <div className="userprofile__thumbnail-info">
+              <h3 className="fs-15-700">{user?.username}</h3>
+              <p className="fs-15-600">{user?.handle}</p>
+            </div>
+            <span className="fs-16-700">...</span>
+          </>
+        )}
+      </div>
+      
+        {isOpen && (
         <>
-          <div className="userprofile__thumbnail-info">
-            <h3 className="fs-15-700">{user?.username}</h3>
-            <p className="fs-15-600">{user?.email}</p>
-          </div>
-          <span className="fs-16-700">...</span>
+        <div className="userprofile__thumbnail-menu">
+          <NavLink to={`/profile/${user?.id}`} onClick={()=> setIsOpen(false)} className="fs-16-700">
+            Profile
+          </NavLink>
+          <a href="/" onClick={handleLogout} className="fs-16-700">
+            Se deconnecter de {user?.handle}
+          </a>
+          <span className="userprofile__thumbnail-menu-square"></span>
+        </div>
+        <div className="userprofile__thumbnail-menu-overlay" onClick={() => setIsOpen(false)}></div>
         </>
       )}
-    </div>
+    </>
   );
 };
 
