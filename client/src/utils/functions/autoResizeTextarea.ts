2ab3d6d42cd5c682aa24dc0415c8d52ff0b6c505
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-16 23:06:19 +0200

    creation fonction pour redimensionner textarea automatiquement

---
+++ client/src/utils/functions/autoResizeTextarea.ts
@@ -0,0 +1,19 @@
+const autoResizeTextarea = (textarea: HTMLTextAreaElement): void => {
+    const lineHeight = 30; // Hauteur approximative d'une ligne (ajuster selon le CSS réel)
+    
+    // Calcul de la nouvelle hauteur en pixels du contenu
+    const scrollHeight = textarea.scrollHeight;
+    
+    // Calcul du nombre actuel de lignes visibles
+    const currentRows = textarea.rows;
+  
+    // Calcul du nombre de lignes nécessaires basé sur le scrollHeight
+    const newRows = Math.floor(scrollHeight / lineHeight);
+    
+    // Ne met à jour les lignes que si le nombre de lignes calculé dépasse celui actuel
+    if (newRows > currentRows) {
+      textarea.rows = newRows;
+    }
+  };
+
+export { autoResizeTextarea };
