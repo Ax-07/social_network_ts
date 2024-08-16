ba276b4c78af9ab59b9b1c69488d037df62cdcf0
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-16 22:45:55 +0200

    ajout preview video youtube

--- client/src/components/addPicture/PreviewPicture.tsx
+++ client/src/components/addPicture/PreviewPicture.tsx
@@ -9,10 +9,21 @@ interface PreviewPictureProps {
 const PreviewPicture: FunctionComponent<PreviewPictureProps> = ({ media, mimetype, onCancel }) => {
   const isPicture = mimetype === 'image/webp' || mimetype === 'image/png' || mimetype === 'image/jpeg';
   const isVideo = mimetype === 'video/mp4';
+  const isYoutubeVideo = mimetype === 'video/youtube';
   return (
     <div className='preview-media'>
       {isPicture && <img src={media} alt="Preview" />}
       {isVideo && <video src={media} controls />}
+      {isYoutubeVideo && (
+        <iframe
+          width="100%"
+          height="315"
+          src={media}
+          title="YouTube video player"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+          allowFullScreen
+        ></iframe>
+      )}
       <button className='btn-close-top-right' type="button" onClick={onCancel}>✖️</button>
     </div>
   );
