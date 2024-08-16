8bb34390f2b1f9ac2f71a00385cc609021e33b21
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-16 22:47:31 +0200

    ajout gestion lien youtube

--- client/src/components/post/PostCard.tsx
+++ client/src/components/post/PostCard.tsx
@@ -14,7 +14,14 @@ const PostCard: FunctionComponent<PostProps> = ({ post }) => {
   const poster = useGetUserByIdQuery(posterId);
   const isWebp = post.media?.endsWith(".webp");
   const isMp4 = post.media?.endsWith(".mp4");
+  const isYoutubeVideo = post.media?.includes("youtube.com");
   const timeSinceCreation = getTimeSinceCreation(post.createdAt as string);
+
+  const embedYoutube = (url: string) => {
+    console.log("url: ", url);
+    const embedVideo = url.replace("watch?v=", "embed/").split('&')[0]; console.log("embedVideo: ", embedVideo);
+    return embedVideo;
+  }
 
   return (
     <article className="post__card">
@@ -45,6 +52,16 @@ const PostCard: FunctionComponent<PostProps> = ({ post }) => {
         )}
         {post.media && isMp4 && (
           <video className="post__card-video" src={post.media} controls />
+        )}
+        {post.media && isYoutubeVideo && (
+          <iframe
+          style={{ width: "100%", aspectRatio: "16/9" }}
+            className="post__card-youtube"
+            src={embedYoutube(post.media)}
+            title="YouTube video player"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
         )}
         <BtnLike post={post} />
       </div>
