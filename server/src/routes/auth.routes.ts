27844b64df3f0726a6e14a8ba624b55d814d527b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-16 23:08:38 +0200

    ajout route refresh token

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { signUp, signIn } from "../controllers/auth.controller";
-import { googleLoginCallback } from "../controllers/googleAuth.controller";
+import { googleLoginCallback, googleRefreshToken } from "../controllers/googleAuth.controller";
 import { googleAuthMiddleware } from "../middleware/googleAuth.middleware";
 
 const router = Router();
@@ -8,6 +8,7 @@ const router = Router();
 router.post('/register', signUp);
 router.post('/login', signIn);
 
-router.post('/google/callback',googleAuthMiddleware, googleLoginCallback);
+router.post('/google/callback', googleAuthMiddleware, googleLoginCallback);
+router.post('/google/refresh-token', googleAuthMiddleware, googleRefreshToken);
 
 export default router;
