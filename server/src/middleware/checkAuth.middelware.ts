b9eb39b89e97036a6b80faeb2e98b41882fbaadf
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-17 23:25:28 +0200

    ajout des fonctions pour gerer les reponses des requetes api

--- server/src/middleware/checkAuth.middelware.ts
+++ server/src/middleware/checkAuth.middelware.ts
@@ -1,11 +1,12 @@
 import { Response, NextFunction } from "express";
 import jwt, { JwtPayload } from 'jsonwebtoken';
 import { CustomRequest } from "../utils/types/customRequest";
+import { apiError } from "../utils/functions/apiResponses";
 
 const checkAuth = async (req: CustomRequest, res: Response, next: NextFunction) => {
     const token = req.headers.authorization?.split(' ')[1]; console.log("token: ", token);
     if (!token) {
-        return res.status(401).json({ message: 'No token provided' });
+        return apiError(res, 'Unauthorized', 'No token provided', 401);
     }
     try {
         const decodedToken = jwt.verify(token, process.env.JWT_SECRET as string) as JwtPayload;  console.log("decodedToken: ", decodedToken);
@@ -13,7 +14,7 @@ const checkAuth = async (req: CustomRequest, res: Response, next: NextFunction) 
         req.auth = userId;
         next();
     } catch (error) {
-        return res.status(401).json({ message: 'Unauthorized' });
+        return apiError(res, 'Unauthorized', 'Invalid token', 401);
     }
 }
 
