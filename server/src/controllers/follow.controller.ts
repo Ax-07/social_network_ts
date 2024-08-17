6fcadc4d54470c6e14d329b98bd54ee750c0c01c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-17 23:23:59 +0200

    ajout des fonctions pour gerer les reponses des requetes api

--- server/src/controllers/follow.controller.ts
+++ server/src/controllers/follow.controller.ts
@@ -1,44 +1,45 @@
 import { Request, Response } from 'express';
 import db from '../models';
 import { handleControllerError } from '../utils/errors/controllers.error';
+import { apiError, apiSuccess } from '../utils/functions/apiResponses';
 
 const followUser = async (req: Request, res: Response) => {
     const { userId, followerId } = req.body;
     if (!userId || !followerId) {
-        return res.status(400).send({ message: 'Missing fields' });
+        return apiError(res, 'Missing fields', 400);
     }
     try {
         const user = await db.User.findByPk(userId);
         const follower = await db.User.findByPk(followerId);
         if (!user || !follower) {
-            return res.status(404).send({ message: 'User not found' });
+            return apiError(res, 'User not found', 404);
         }
         await user.update({ followers: [...(user.followers || []), followerId] });
         await follower.update({ followings: [...follower.followings || [], userId] });
 
-        res.status(200).send({ message: 'User followed successfully' });
+        return apiSuccess(res, 'User followed successfully', { userId, followerId });
     } catch (error) {
-        handleControllerError(res, error, 'An error occurred while following the user.');
+        return handleControllerError(res, error, 'An error occurred while following the user.');
     }
 };
 
 const unfollowUser = async (req: Request, res: Response) => {
     const { userId, followerId } = req.body;
     if (!userId || !followerId) {
-        return res.status(400).send({ message: 'Missing fields' });
+        return apiError(res, 'Missing fields', 400);
     }
     try {
         const user = await db.User.findByPk(userId);
         const follower = await db.User.findByPk(followerId);
         if (!user || !follower) {
-            return res.status(404).send({ message: 'User not found' });
+            return apiError(res, 'User not found', 404);
         }
         await user.update({ followers: user.followers?.filter((id) => id !== followerId) });
         await follower.update({ followings: follower.followings?.filter((id) => id !== userId) });
 
-        res.status(200).send({ message: 'User unfollowed successfully' });
+        return apiSuccess(res, 'User unfollowed successfully', { userId, followerId });
     } catch (error) {
-        handleControllerError(res, error, 'An error occurred while unfollowing the user.');
+        return handleControllerError(res, error, 'An error occurred while unfollowing the user.');
     }
 };
 
