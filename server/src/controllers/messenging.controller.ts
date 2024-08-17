4dd96a8f7998fce5509424025a9db37467b60176
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-17 23:25:01 +0200

    ajout des fonctions pour gerer les reponses des requetes api

--- server/src/controllers/messenging.controller.ts
+++ server/src/controllers/messenging.controller.ts
@@ -1,11 +1,11 @@
 import { Request, Response } from 'express';
+import { apiError, apiSuccess } from '../utils/functions/apiResponses';
 
 const uploadFileMessenging = async (req: Request, res: Response) => {
     if (!res.locals.filePath) {
-        return res.status(400).json({ error: 'No file uploaded' });
+        return apiError(res, 'Validation error', 'File path is required', 400);
     }
-
-    return res.status(201).json({ filePath: res.locals.filePath });
+    return apiSuccess(res, 'File uploaded successfully', { filePath: res.locals.filePath });
 }
 
 export default uploadFileMessenging;
