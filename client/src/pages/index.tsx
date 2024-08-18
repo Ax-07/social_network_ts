ee8bad7f155ec0eec9368fd8983abad1c2bf4cc0
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-18 23:28:11 +0200

    creation fichier index pour recuperer toutes les pages

---
+++ client/src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Home from './home/Home';
+import Explore from './explore/Explore';
+import Notifications from './notifications/Notifications';
+import Messages from './messages/Messages';
+import BooKmarks from './bookmarks/BooKmarks';
+import Lists from './lists/Lists';
+import Profile from './profile/Profile';
+import More from './more/More';
+import AuthPage from './auth/AuthPage';
+
+const Pages = {
+  Home,
+  Explore,
+  Notifications,
+  Messages,
+  BooKmarks,
+  Lists,
+  Profile,
+  More,
+  AuthPage,
+};
+
+export default Pages;
