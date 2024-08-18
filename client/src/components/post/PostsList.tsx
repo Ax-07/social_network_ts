3aba5e62474f7b817261ee9bcf265c885e529ad1
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-18 23:25:09 +0200

    refactoring et ajout d'une key a postcard

--- client/src/components/post/PostsList.tsx
+++ client/src/components/post/PostsList.tsx
@@ -2,23 +2,24 @@ import { useGetPostsQuery } from "../../services/api/postApi";
 import PostCard from "./PostCard";
 
 const PostList = () => {
-    const { data: posts = [], isLoading, isError } = useGetPostsQuery();
-    const sortedPosts = [...posts].sort((a, b) => {
-        return new Date(b.createdAt ?? 0).getTime() - new Date(a.createdAt ?? 0).getTime();
-    });
-    return (
-        <div className="post">
-            <ul className="post__list">
-                {isLoading && <p>Loading...</p>}
-                {isError && <p>Error</p>}
-                {sortedPosts.map((post) => (
-                    <li className="post__item" key={post.id}>
-                        <PostCard post={post} />
-                    </li>
-                ))}
-            </ul>
-        </div>
-    );
+  const { data: posts, isLoading, isError } = useGetPostsQuery();
+  if (isLoading) return <p>Loading...</p>;
+  if (isError) return <p>Error</p>;
+  const sortedPosts = [...(posts?.data ?? [])].sort((a, b) => {
+    return new Date(b.createdAt ?? 0).getTime() - new Date(a.createdAt ?? 0).getTime();
+  });
+
+  return (
+    <div className="post">
+      <ul className="post__list">
+        {sortedPosts.map((post) => (
+          <li className="post__item" key={post.id}>
+            <PostCard key={post.id} post={post} />
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default PostList;
