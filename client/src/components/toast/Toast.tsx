1e23a80b3e7a917871a16ba787d82b25cd518a41
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-18 23:22:34 +0200

    creation de toast de notification

---
+++ client/src/components/toast/Toast.tsx
@@ -0,0 +1,78 @@
+import type { FunctionComponent } from "react";
+import successIcon from "../../assets/icons/faCheckCircle.svg";
+import warningIcon from "../../assets/icons/faCircleExclamation.svg";
+import infoIcon from "../../assets/icons/faInfoCircle.svg";
+import errorIcon from "../../assets/icons/faCircleXmark.svg";
+
+export interface ToastType {
+  type: "success" | "error" | "info" | "warning" | "default";
+  message: string;
+}
+
+const Toast: FunctionComponent<ToastType> = ({ type, message }) => {
+  let icon = "";
+  let title = "";
+  let name = "";
+  let color = "";
+
+  switch (type) {
+    case "success":
+      {
+        (icon = successIcon),
+        (title = "Success"),
+        (name = "success"),
+        (color = "#00DF80");
+      }
+      break;
+    case "error":
+      {
+        (icon = errorIcon),
+        (title = "Error"),
+        (name = "error"),
+        (color = "#F04248");
+      }
+      break;
+    case "info":
+      {
+        (icon = infoIcon),
+        (title = "Info"),
+        (name = "info"),
+        (color = "#1d9bf0");
+      }
+      break;
+    case "warning":
+      {
+        (icon = warningIcon),
+        (title = "Warning"),
+        (name = "warning"),
+        (color = "#FFD21E");
+      }
+      break;
+    default: {
+      (icon = "default"), (title = "Default"), (name = "default"), (color = "#fff");
+    }
+  }
+
+  return (
+    <div className="toast" style={{boxShadow: `3px 3px 10px ${color}`}}>
+      <div className={`toast__icon-wrapper toast__icon-wrapper-${name}`}>
+      </div>
+        <img
+          className="toast__icon"
+          src={icon}
+          alt={`icon ${name}`}
+        />
+      <div className="toast__content">
+        <h3 className="toast__title">{title}</h3>
+        <p className="toast__message">{message}</p>
+      </div>
+      <div
+        className="toast__progress"
+        style={{ background: `${color}` }}
+      ></div>
+    
+    </div>
+  );
+};
+
+export default Toast;
---
+++ client/src/components/toast/ToastContext.tsx
@@ -0,0 +1,45 @@
+// ToastsContext.tsx
+import { createContext, PropsWithChildren, useContext, useState, useRef, useCallback } from "react";
+import Toast from "./Toast";
+import { ToastType } from "./Toast";
+
+export const ToastsContext = createContext<ToastType[]>([]);
+export const PushToastContext = createContext<{ current: (toast: ToastType) => void }>({ current: () => {} });
+
+export const ToastsProvider = ({ children }: PropsWithChildren) => {
+  const [toasts, setToasts] = useState<ToastType[]>([]);
+  const pushToastRef = useRef<(toast: ToastType) => void>(() => {});
+
+  // Fonction d'ajout d'un toast
+  const pushToast = useCallback((toast: ToastType) => {
+    setToasts((prevToasts) => [...prevToasts, toast]);
+    setTimeout(() => {
+      setToasts((prevToasts) => prevToasts.filter((t) => t !== toast));
+    }, 5000);
+  }, []);
+
+  // Mise à jour de la référence avec la fonction pushToast
+  pushToastRef.current = pushToast;
+
+  return (
+    <PushToastContext.Provider value={pushToastRef}>
+      <ToastsContext.Provider value={toasts}>
+        <Toasts />
+        {children}
+      </ToastsContext.Provider>
+    </PushToastContext.Provider>
+  );
+};
+
+const Toasts = () => {
+  const toasts = useContext(ToastsContext);
+  return (
+    <div className="toasts">
+      {toasts.map((toast, index) => (
+        <Toast key={index} {...toast} />
+      ))}
+    </div>
+  );
+};
+
+
