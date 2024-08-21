4bef0decc308c3b0996bba8949938ea296370497
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-22 00:17:37 +0200

    delete tablist et ajout des routes pour PostPage et CommentPage

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,24 +1,19 @@
-import TabList from "../../components/tabList/TabList";
 import { Routes, Route } from "react-router-dom";
-import AddPost from "../../components/post/AddPost";
 import PostList from "../../components/post/PostsList";
+import PostPage from "../post/PostPage";
+import CommentPage from "../post/CommentPage";
 
 const Home = () => {
   return (
-    <div className="home">
-      <TabList
-        links={[
-          { name: "Pour vous", to: "/home/posts" },
-          { name: "Abonnements", to: "/home/abonnements" },
-        ]}
-      />
-      <AddPost origin="page" />
+    <section className="home">
       <Routes>
+        <Route path="/posts/:id" element={<PostPage />} />
+        <Route path="/comment/:id" element={<CommentPage />} />
         <Route path="/posts" element={<PostList />} />
         <Route path="/abonnements" element={<h2>Abonnements</h2>} />
         <Route path="/coding-lab" element={<h2>CodingLab</h2>} />
       </Routes>
-    </div>
+    </section>
   );
 };
 
