279e9a472131f54f1505476f56ba5ac4308f7683
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-22 00:27:26 +0200

    remplacement des import des pages par le composant Pages et ajout du composant Modals qui gere toutes les modals

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,28 +1,17 @@
 import { Routes, Route, Navigate  } from "react-router-dom";
 import SideMenu from "./layouts/sideMenu/SideMenu";
 import SideColumn from "./layouts/sideColumn/SideColumn";
-import Home from "./pages/home/Home";
-import Explore from "./pages/explore/Explore";
-import Notifications from "./pages/notifications/Notifications";
-import Messages from "./pages/messages/Messages";
-import BooKmarks from "./pages/bookmarks/BooKmarks";
-import Lists from "./pages/lists/Lists";
-import Profile from "./pages/profile/Profile";
-import More from "./pages/more/More";
 import { useWindowSize } from "./utils/hooks/useWindowSize";
-import Modal from "./components/modal/Modal";
-import AddPost from "./components/post/AddPost";
-import {useModal} from "./components/modal/useModal"; // Assurez-vous que le chemin est correct
-import AuthPage from "./pages/auth/AuthPage";
 import { useEffect } from "react";
 import { useGoogleRefreshTokenMutation } from "./services/auth/googleAuthApi";
 import { useSelector } from "react-redux";
 import { RootState } from "./services/stores";
+import Pages from "./pages";
+import Modals from "./components/modal/Modals";
 
 function App() {
   const { windowWidth } = useWindowSize();
   const isTablet = windowWidth <= 1020;
-  const { modals } = useModal();
   const refreshToken = useSelector((state: RootState) => state.auth?.refreshToken);
   const isAuth = useSelector((state: RootState) => state.auth?.isAuthenticated);
   const [ googleRefreshToken ] = useGoogleRefreshTokenMutation();
@@ -49,22 +38,18 @@ function App() {
       <SideMenu />
       <main>
         <Routes>
-        <Route path="/" element={<Navigate to="/home/posts" />} />
-          <Route path="/auth" element={<AuthPage />} />
-          <Route path="/home/*" element={<Home />} />
-          <Route path="/explore" element={<Explore />} />
-          <Route path="/notifications" element={<Notifications />} />
-          <Route path="/messages" element={<Messages />} />
-          <Route path="/bookmarks" element={<BooKmarks />} />
-          <Route path="/lists" element={<Lists />} />
-          <Route path="/profile/:id/*" element={<Profile />} />
-          <Route path="/more" element={<More />} />
+          <Route path="/" element={<Navigate to="/home/posts" />} />
+          <Route path="/auth" element={<Pages.AuthPage />} />
+          <Route path="/home/*" element={<Pages.Home />} />
+          <Route path="/explore" element={<Pages.Explore />} />
+          <Route path="/notifications" element={<Pages.Notifications />} />
+          <Route path="/messages" element={<Pages.Messages />} />
+          <Route path="/bookmarks" element={<Pages.BooKmarks />} />
+          <Route path="/lists" element={<Pages.Lists />} />
+          <Route path="/profile/:id/*" element={<Pages.Profile />} />
+          <Route path="/more" element={<Pages.More />} />
         </Routes>
-        {modals && (
-          <Modal modalName="Post">
-            <AddPost origin="modal"/>
-          </Modal>
-        )}
+        <Modals />
       </main>
         {!isTablet && <SideColumn />}
     </div>
