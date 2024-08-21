c626c587c0402e540c7c7c187203a67f9299dde5
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-22 00:33:04 +0200

    ajout des routes pour recuperer les commentaire des posts ous des commentaires

--- server/src/routes/comment.routes.ts
+++ server/src/routes/comment.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createComment, deleteComment, getAllComments, getCommentById, updateComment } from "../controllers/comment.controller";
+import { createComment, deleteComment, getAllComments, getCommentById, getCommentsByCommentId, getCommentsByPostId, updateComment } from "../controllers/comment.controller";
 import uploadFileMiddleware from "../middleware/multer.middleware";
 import { checkAuth } from "../middleware/checkAuth.middelware";
 
@@ -7,6 +7,8 @@ const router = Router();
 
 router.post('/comments', checkAuth, uploadFileMiddleware, createComment);
 router.get('/comments', getAllComments);
+router.get('/posts/comments/:postId', getCommentsByPostId);
+router.get('/comments/comments/:commentId', getCommentsByCommentId);
 router.get('/comments/:id', getCommentById);
 router.patch('/comments/:id', checkAuth, updateComment);
 router.delete('/comments/:id', checkAuth, deleteComment);
