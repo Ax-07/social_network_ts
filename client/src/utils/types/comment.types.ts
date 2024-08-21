583c6c35d9b6addc60dea99d99fc7a5c7c227693
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-22 00:24:16 +0200

    creation de type pour commentApi

---
+++ client/src/utils/types/comment.types.ts
@@ -0,0 +1,23 @@
+export interface CommentTypes {
+  id: string;
+  postId: string;
+  userId: string;
+  content: string;
+  media?: string | null;
+  likers?: string[];
+  commentsCount?: number;
+  createdAt?: string;
+  updatedAt?: string;
+}
+
+export interface CommentResponseArray {
+  status: string;
+  data: CommentTypes[];
+  message: string;
+}
+
+export interface CommentResponse {
+  status: string;
+  data: CommentTypes;
+  message: string;
+}
