2b7715e81e8528bfcfb1c5738afbc3416d01f82d
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-22 00:07:50 +0200

    creation composant pour l'ajout de commentaire

---
+++ client/src/components/post/AddComment.tsx
@@ -0,0 +1,77 @@
+import PostForm from './PostForm';
+import { usePostFormContext } from './hooks/usePostFormContext';
+import { useAddCommentMutation } from '../../services/api/commentApi';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../services/stores';
+import { useParams } from 'react-router-dom';
+import { usePushToast } from '../toast/Toasts';
+import { ApiError } from '../../utils/types/api.types';
+import { useModal } from '../modal/hook/useModal';
+
+interface AddCommentProps{
+  origin: "modal-addPost" | "modal-comment" | "page-home" | "post-page-comment" | "comment-page-comment" | "post-list" | "modal-comment-post" | "modal-comment-comment";
+  onClose?: () => void;
+}
+
+const AddComment = ({ origin, onClose }: AddCommentProps) => {
+  const postIdFromParams = useParams<{ id: string }>().id;
+  const { postId, commentId } = useModal();
+  const userId = useSelector((state: RootState) => state.auth.user?.id);
+  const postForm = usePostFormContext();
+  const [addComment, { isLoading }] = useAddCommentMutation();
+  const pushToast = usePushToast();
+
+  console.log('origin', origin);
+  let formKey;
+  let id;
+  switch (origin) {
+    case "modal-comment-post":
+      formKey = "postId";
+      id = postId;
+      break;
+    case "modal-comment-comment":
+      formKey = "commentId";
+      id = commentId;
+      break;
+    case "post-page-comment":
+      formKey = "postId";
+      id = postIdFromParams;
+      break;
+    case "comment-page-comment":
+      formKey = "commentId";
+      id = commentId;
+      break;
+    default:
+      formKey = "postId";
+      id = postId;
+  }
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append(formKey, id as string);
+      formData.append("userId", userId as string);
+      formData.append("content", postForm.form.content);
+      if (typeof postForm.form.file === "string") {
+        formData.append("media", postForm.form.file);
+      } else {
+        formData.append("media", postForm.form.file as Blob);
+      }
+
+      const response = await addComment({formData: formData , origin}).unwrap();
+      pushToast({ message: response.message, type: "success" });
+    } catch (error) {
+      pushToast({ message: (error as ApiError).data.message, type: "error" });
+    } finally {
+      postForm.resetForm();
+      onClose && onClose();
+    }
+  };
+
+  if (isLoading) return <p>Publication en cours...</p>;
+
+  return <PostForm handleSubmit={handleSubmit} origin={origin} />
+};
+
+export default AddComment;
