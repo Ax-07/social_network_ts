6c0ab6e80863398b28ae990e5492481a22f38b9b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-22 00:18:20 +0200

    creation comment page

---
+++ client/src/pages/post/CommentPage.tsx
@@ -0,0 +1,34 @@
+import { NavLink, useParams } from "react-router-dom";
+import { useGetCommentByIdQuery } from "../../services/api/commentApi";
+import CommentCard from "../../components/post/CommentCard";
+import AddComment from "../../components/post/AddComment";
+import CommentList from "../../components/post/CommentList";
+import { PostFormProvider } from "../../components/post/context/postFormContext";
+
+const CommentPage = () => {
+    const { id } = useParams<{ id: string }>(); console.log(id);
+    const { data: comment, error, isLoading } = useGetCommentByIdQuery(id as string); console.log(comment);
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error: {(error as Error).message}</div>;
+
+    // Vérifiez si `comment` est défini avant de rendre `CommentCard`
+    if (!comment) return <div>No comment found</div>;
+
+    return (
+        <section className="post-page">
+            <div>
+            <NavLink to={"/home"}>
+                <img src="/src/assets/icons/faArrowLeft.svg" alt="icon arrow left" />
+            </NavLink>
+            </div>
+            <CommentCard comment={comment.data} origin="comment-page"/>
+            <PostFormProvider origin="comment-page-comment">
+                <AddComment origin="comment-page-comment" />
+            </PostFormProvider>
+            <CommentList commentId={id}/>
+        </section>
+    );
+};
+
+export default CommentPage;
