8e3ccaa7730a99de735c6a2dcba1aa156a14f43c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-22 00:09:28 +0200

    ajout du composant postForm et de son hook

--- client/src/components/post/AddPost.tsx
+++ client/src/components/post/AddPost.tsx
@@ -1,54 +1,23 @@
-import { useState, useRef, useCallback } from "react";
 import { useAddPostMutation } from "../../services/api/postApi";
-import { UserPicture } from "../userProfile/UserProfileThumbnail";
-import PreviewPicture from "../addPicture/PreviewPicture";
-import InputPicture from "../addPicture/InputPicure"; // Correction de la typo InputPicure
-import Button from "../button/Button";
-import { useSelector } from "react-redux";
-import { RootState } from "../../services/stores";
-import { autoResizeTextarea } from "../../utils/functions/autoResizeTextarea";
 import { usePushToast } from "../toast/Toasts";
-
-interface FormState {
-  userId: string | undefined;
-  content: string;
-  file: File | string;
-}
+import { ApiError } from "../../utils/types/api.types";
+import PostForm from "./PostForm";
+import { usePostFormContext } from "./hooks/usePostFormContext";
 
 interface AddPostProps {
-  origin: "modal" | "page";
+  origin: "modal-addPost" | "modal-comment" | "page-home" | "post-page-comment" | "comment-page-comment";
   onClose?: () => void;
 }
 
-
 const AddPost = ({ origin, onClose }: AddPostProps) => {
-  const userId = useSelector((state: RootState) => state?.auth?.user?.id);
-  const [form, setForm] = useState<FormState>({
-    userId: userId,
-    content: "",
-    file: "",
-  });
-
-  const [preview, setPreview] = useState<string>("");
-  const [mimetype, setMimetype] = useState<string>("");
-
-  const inputFileRef = useRef<HTMLInputElement>(null); // Référence pour l'entrée de fichier
-
-  const isPreview = form.file !== "";
-  const isValidForm = form.content.trim().length > 0;
+  const {
+    form,
+    isValidForm,
+    resetForm,
+  } = usePostFormContext();
 
   const [addPost, { isLoading }] = useAddPostMutation();
   const pushToast = usePushToast();
-
-  const resetForm = useCallback(() => {
-    setForm({
-      userId: userId,
-      content: "",
-      file: "",
-    });
-    setPreview("");
-    setMimetype("");
-  }, [userId]);
 
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -63,98 +32,18 @@ const AddPost = ({ origin, onClose }: AddPostProps) => {
       }
 
       const response = await addPost(formData).unwrap();
-      if( response.status !== 201) {
-          pushToast({ message: response.message, type: "error" });
-        }
       pushToast({ message: response.message, type: "success" });
     } catch (error) {
-      pushToast({ message: (error as Error).message, type: "error" });
+      pushToast({ message: (error as ApiError).data.message, type: "error" });
     } finally {
       resetForm();
       onClose && onClose();
     }
   };
-
-  const handleFileChange = useCallback((image: File) => {
-    setForm((prevForm) => ({ ...prevForm, file: image }));
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      setPreview(reader.result as string);
-      setMimetype(image.type);
-    };
-    reader.readAsDataURL(image);
-  }, []);
-
-  const embedYoutube = (url: string) => {
-    console.log("url: ", url);
-    const embedVideo = url.replace("watch?v=", "embed/").split('&')[0];
-    return embedVideo;
-  }
-
-  const handleContentChange = useCallback((e: React.ChangeEvent<HTMLTextAreaElement>) => {
-    const content = e.target.value;
-    if (content.includes('youtube.com')) {
-      const youtubeUrl = embedYoutube(content);
-      setPreview(youtubeUrl);
-      setForm((prevForm) => ({ ...prevForm, content, file: youtubeUrl }));
-      setMimetype("video/youtube");
-    } else {
-      setForm((prevForm) => ({ ...prevForm, content }));
-      setPreview("");
-    }
-  }, []);
-
+console.log("AddPost", {isValidForm});  // Affiche bien isValidForm
   if (isLoading) return <p>Publication en cours...</p>;
   
-  return (
-    <div className={`addpost addpost--${origin}`}>
-      <div className="addpost__avatar">
-        <UserPicture />
-      </div>
-      <form onSubmit={handleSubmit} className="addpost__form">
-        <div className="addpost__header">
-          <select name="" id="">
-            <option value="public">Public</option>
-            <option value="private">Privé</option>
-          </select>
-        </div>
-        <textarea
-          className="addpost__input"
-          placeholder="Quoi de neuf ?"
-          name="content"
-          id="content"
-          rows={1}
-          onChange={handleContentChange}
-          value={form.content}
-          onInput={(e) =>  autoResizeTextarea(e.currentTarget)}
-        />
-        {isPreview && (
-          <PreviewPicture
-            media={preview}
-            mimetype={mimetype}
-            onCancel={() => {
-              setForm({ ...form, file: "" });
-              setPreview("");
-              setMimetype("");
-              if (inputFileRef.current) {
-                inputFileRef.current.value = ""; // Réinitialiser la valeur de l'entrée de fichier
-              }
-            }}
-          />
-        )}
-        <div className="addpost__bottom">
-          <InputPicture setMedia={handleFileChange} inputRef={inputFileRef} />
-          <Button
-            type="submit"
-            className="btn__post-submit"
-            disabled={!isValidForm}
-          >
-            Publier
-          </Button>
-        </div>
-      </form>
-    </div>
-  );
+  return <PostForm handleSubmit={handleSubmit} origin={origin} />
 };
 
 export default AddPost;
