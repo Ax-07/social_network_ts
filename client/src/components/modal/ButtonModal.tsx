852129d4619b01c279bed231a743e51d7277a72b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-23 22:48:30 +0200

    ajout des types a modalName, et creation d'une fonction handleOpen

--- client/src/components/modal/ButtonModal.tsx
+++ client/src/components/modal/ButtonModal.tsx
@@ -1,24 +1,27 @@
 import { FunctionComponent, PropsWithChildren } from 'react';
 import { useModal } from './hook/useModal';
 import Button from '../button/Button';
+import { PostFormOrigin } from '../post/PostForm';
 
 interface ButtonModalProps extends PropsWithChildren {
-  modalName: string;
+  modalName: PostFormOrigin;
   postId?: string;
   commentId?: string;
+  className?: string;
 }
 
-const ButtonModal: FunctionComponent<ButtonModalProps> = ({ modalName, postId, commentId, children }) => {
+const ButtonModal: FunctionComponent<ButtonModalProps> = ({ modalName, postId, commentId, children, className }) => {
   const { openModal, setModalName, setPostId, setCommentId } = useModal();
 
+  const handleOpen = () => {
+    openModal(modalName);
+    setModalName(modalName);
+    setPostId(postId ?? '');
+    setCommentId(commentId ?? '');
+  };
   return (
-    <Button type="button" className="btn__post-modal" 
-      onClick={() => {
-        openModal(modalName);
-        setModalName(modalName);
-        setPostId(postId ?? '');
-        setCommentId(commentId ?? '');
-      }}>
+    <Button type="button" className={className} 
+      onClick={handleOpen}>
       {children}
     </Button>
   );
