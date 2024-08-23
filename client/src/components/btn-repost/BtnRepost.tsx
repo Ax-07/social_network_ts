2e1501bbe10bbd82a1dbdd1d59221dad3e1dd3e5
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-23 22:44:32 +0200

    creation btnRepost

---
+++ client/src/components/btn-repost/BtnRepost.tsx
@@ -0,0 +1,70 @@
+import { useState, useRef, useEffect, type FunctionComponent } from "react";
+import ReactDOM from "react-dom";
+import { useModal } from "../modal/hook/useModal";
+
+interface BtnRepostProps {
+  postId?: string;
+  commentId?: string;
+}
+
+const BtnRepost: FunctionComponent<BtnRepostProps> = ({ postId }) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
+  const buttonRef = useRef<HTMLDivElement>(null);
+  const { modals, openModal, closeModal } = useModal();
+
+  useEffect(() => {
+    if (modals['btn-repost'] && buttonRef.current) {
+      const rect = buttonRef.current.getBoundingClientRect();
+      setModalPosition({
+        top: rect.bottom + window.scrollY - 50, // Position sous le bouton
+        left: rect.left + window.scrollX - 50  // Aligné à gauche du bouton
+      });
+    }
+  }, [modals]);
+
+  const handleOpen = () => {
+    setIsOpen(true);
+    openModal('btn-repost'); // Ouvrir la modal
+  };
+
+  useEffect(() => {
+    if (!modals['btn-repost']) {
+      setIsOpen(false);
+    }
+  }, [modals]);
+
+  const handleClose = () => {
+    setIsOpen(false);
+    closeModal('btn-repost'); // Fermer la modal
+  };
+
+  return (
+    <>
+      <div ref={buttonRef} className="btn-repost__primary" onClick={handleOpen}>
+        <img src="/src/assets/icons/faRetweet.svg" alt="icon retweet" />
+        <p>0</p>
+      </div>
+
+      {isOpen &&
+        ReactDOM.createPortal(
+          <div
+            className="btn-repost__modal"
+            style={{ position: 'absolute', top: modalPosition.top, left: modalPosition.left }}
+          >
+            <div className="btn-repost__modal-option" onClick={handleClose}>
+              <img src="/src/assets/icons/faRetweet.svg" alt="icon retweet" />
+              <p>Repost</p>
+            </div>
+            <div className="btn-repost__modal-option" onClick={handleClose}>
+              <img src="/src/assets/icons/faPencilAlt.svg" alt="icon pen" />
+              <p>Citation</p>
+            </div>
+          </div>,
+          document.getElementById('btn-repost-modal') || document.body
+        )}
+    </>
+  );
+};
+
+export default BtnRepost;
