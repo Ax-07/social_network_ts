a3d47c5ed7d6fbec1c3febfe23ef89fca397d46d
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-23 22:50:42 +0200

    ajout des types a modalName, creation d'une fonction handleOverlayClick pour fermer la modal et ajout d'une fonction handleContentClock avec un stopPropagation pour eviter de fermer la modal en clickant dessus

--- client/src/components/modal/Modal.tsx
+++ client/src/components/modal/Modal.tsx
@@ -1,20 +1,30 @@
 import type { FunctionComponent, PropsWithChildren } from 'react';
 import { useModal } from './hook/useModal';
+import { PostFormOrigin } from '../post/PostForm';
 
 interface ModalProps extends PropsWithChildren<{
-  modalName: string;
+  modalName: PostFormOrigin;
 }> {}
 
 const Modal: FunctionComponent<ModalProps> = ({ children, modalName }) => {
   const { modals, closeModal } = useModal();
   if (!modals[modalName]) return null;
 
+  const handleOverlayClick = () => {
+    closeModal(modalName);
+  };
+
+  const handleContentClick = (e: React.MouseEvent) => {
+    e.stopPropagation(); // Empêche la propagation de l'événement à l'overlay
+  };
+
   return (
-    <div className="modal-overlay">
-      <div className="modal-content">
+    <div className="modal-overlay" onClick={handleOverlayClick}>
+      {modalName !== 'btn-repost' &&
+      <div className="modal-content" onClick={handleContentClick}>
         <button onClick={()=> closeModal(modalName)} className="modal-close">❌</button>
         {children}
-      </div>
+      </div>}
     </div>
   );
 };
