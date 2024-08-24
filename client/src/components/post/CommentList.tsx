20502aef4c31e494d8f4de3d16a66f0bdf533d8f
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-24 21:54:07 +0200

    ajout d'un useeffect pour set commentedPostId et commentedCommentId, ajout de la logique pour afficher les commentaire du plus recent au plus ancien

--- client/src/components/post/CommentList.tsx
+++ client/src/components/post/CommentList.tsx
@@ -1,6 +1,7 @@
-import { FunctionComponent } from "react";
+import { FunctionComponent, useEffect } from "react";
 import { useGetCommentsByCommentIdQuery, useGetCommentsByPostIdQuery } from "../../services/api/commentApi";
 import CommentCard from "./CommentCard";
+import { useModal } from "../modal/hook/useModal";
 
 interface CommentListProps {
     postId?: string | undefined;
@@ -10,17 +11,32 @@ interface CommentListProps {
 const CommentList: FunctionComponent<CommentListProps> = ({postId, commentId}) => {
     const { data: postComments, isLoading: isLoadingPostComments, isError: isErrorPostComments } = useGetCommentsByPostIdQuery(postId ?? "");
     const { data: commentComments, isLoading: isLoadingCommentComments, isError: isErrorCommentComments } = useGetCommentsByCommentIdQuery(commentId ?? "");
+    const { setCommentedPostId, setCommentedCommentId } = useModal();
+
+    useEffect(() => {
+    if (postId) {
+        setCommentedPostId(postId ?? "");
+    } else {
+        setCommentedCommentId(commentId ?? "");
+    }
+    }, [postId, commentId, setCommentedPostId, setCommentedCommentId]);
 
     // Déterminer quelle donnée afficher
     const comments = postId && !commentId ? postComments : commentComments;
     const isLoading = postId && !commentId ? isLoadingPostComments : isLoadingCommentComments;
     const isError = postId && !commentId ? isErrorPostComments : isErrorCommentComments;
+    
+    const sortedComments = [...(comments?.data ?? [])].sort((a, b) => {
+        return new Date(b.createdAt ?? 0).getTime() - new Date(a.createdAt ?? 0).getTime();
+    });
 
     if (isLoading) return <p>Loading...</p>;
     if (isError) return <p>Error</p>;
+
+    console.log('comments', comments);
     return (
         <ul className="post__list">
-            {comments?.data.map((comment) => (
+            {sortedComments.map((comment) => (
                 <li className="post__item" key={comment.id}>
                     <CommentCard key={comment.id} comment={comment} origin="post-page"/>
                 </li>
