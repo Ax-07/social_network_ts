e99b8a74c0a76057f3f57f5e0c2c245b402d1613
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-24 21:59:35 +0200

    creation fonction getFormatedDate et ajout de jsdoc

--- client/src/utils/functions/formatedDate.ts
+++ client/src/utils/functions/formatedDate.ts
@@ -1,4 +1,10 @@
 // Définir une fonction pour formater la différence de temps
+/**
+ * @param differenceInMilliseconds - Différence en millisecondes
+ * @returns Différence de temps formatée
+ * @example
+ * // returns "2 days ago"
+ */
 const formatDifference = (differenceInMilliseconds: number): string => {
   const seconds = Math.floor(differenceInMilliseconds / 1000);
   const minutes = Math.floor(seconds / 60);
@@ -17,6 +23,12 @@ const formatDifference = (differenceInMilliseconds: number): string => {
 };
 
 // Définir une fonction pour calculer le temps écoulé depuis la date de création
+/**
+ * @param createdAt - Date de création au format ISO 8601
+ * @returns Temps écoulé depuis la date de création
+ * @example
+ * // returns "2 days ago"
+ */
 export const getTimeSinceCreation = (createdAt: string): string => {
   const currentDateTime = new Date();
   const postDateTime = new Date(createdAt);
@@ -24,4 +36,27 @@ export const getTimeSinceCreation = (createdAt: string): string => {
   const differenceInMilliseconds =
     currentDateTime.getTime() - postDateTime.getTime();
   return formatDifference(differenceInMilliseconds);
+};
+
+
+// definir une fonction pour formater la date au format dd/mm/yyyy
+/**
+ * @param date - Date au format ISO 8601
+ * @returns Date formatée au format dd/mm/yyyy
+ * @example
+ * // returns "01/01/2022"
+ */
+export const getFormattedDate = (date: string) => {
+  const formattedDate = new Date(date);
+  const day = formattedDate.getDate();
+  const getMonth = () => {
+    if (formattedDate.getMonth() + 1 < 10) {
+      return `0${formattedDate.getMonth() + 1}`;
+    } else {
+      return formattedDate.getMonth() + 1;
+    }
+  }
+  const month = getMonth();
+  const year = formattedDate.getFullYear();
+  return `${day}/${month}/${year}`;
 };
