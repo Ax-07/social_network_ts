b8ee65bc14ada729653c8384e090ecc16a42c6e5
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-24 21:40:17 +0200

    ajout state pour commentedPostId et commentedCommentId

--- client/src/components/modal/context/ModalContext.tsx
+++ client/src/components/modal/context/ModalContext.tsx
@@ -14,6 +14,10 @@ export interface ModalContextType {
   setPostId: (postId: string) => void;
   commentId?: string;
   setCommentId: (commentId: string) => void;
+  commentedPostId?: string;
+  setCommentedPostId: (commentedPostId: string) => void;
+  commentedCommentId?: string;
+  setCommentedCommentId: (commentedCommentId: string) => void;
 }
 
 export const ModalContext = createContext<ModalContextType | undefined>(undefined);
@@ -24,11 +28,21 @@ export const ModalProvider = ({ children }: { children: ReactNode }
   const [modals, setModals] = useState<ModalState>({});
   const [postId, setPostId] = useState<string>('');
   const [commentId, setCommentId] = useState<string>('');
+  const [commentedPostId, setCommentedPostId] = useState<string>('');
+  const [commentedCommentId, setCommentedCommentId] = useState<string>('');
   const openModal = (modalName: string) => setModals(prev => ({ ...prev, [modalName]: true }));
   const closeModal = (modalName: string) => setModals(prev => ({ ...prev, [modalName]: false }));
 
   return (
-    <ModalContext.Provider value={{ modals, modalName, setModalName, commentId, setCommentId, postId, setPostId , openModal, closeModal }}>
+    <ModalContext.Provider value={{ 
+      modals, 
+      modalName, setModalName, 
+      commentId, setCommentId, 
+      postId, setPostId, 
+      commentedPostId, setCommentedPostId, 
+      commentedCommentId, setCommentedCommentId, 
+      openModal, closeModal 
+      }}>
       {children}
     </ModalContext.Provider>
   );
