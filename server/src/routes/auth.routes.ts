376836fb04be44edec9153b6d6756de131fc81da
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-25 22:54:44 +0200

    modif nom des fonctions d'auth

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -1,12 +1,12 @@
 import { Router } from "express";
-import { signUp, signIn } from "../controllers/auth.controller";
+import { register, login } from "../controllers/auth.controller";
 import { googleLoginCallback, googleRefreshToken } from "../controllers/googleAuth.controller";
 import { googleAuthMiddleware } from "../middleware/googleAuth.middleware";
 
 const router = Router();
 
-router.post('/register', signUp);
-router.post('/login', signIn);
+router.post('/register', register);
+router.post('/login', login);
 
 router.post('/google/callback', googleAuthMiddleware, googleLoginCallback);
 router.post('/google/refresh-token', googleAuthMiddleware, googleRefreshToken);
