25c9a1e070fca144702e493985f3b14cdc357f5a
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-25 22:48:58 +0200

    creation fonction pour generer un username a l'inscription

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -3,34 +3,39 @@ import jwt from 'jsonwebtoken';
 import db from '../models';
 import { handleControllerError } from '../utils/errors/controllers.error';
 import validateUserEntry from '../utils/functions/validateUserEntry';
-import generateUniqueHandle from '../utils/functions/generateUniqueHandle';
 import { apiError, apiSuccess } from '../utils/functions/apiResponses';
+import generateUsernameFromEmail from '../utils/functions/generateUsernameFromEmail';
+import generateUniqueHandle from '../utils/functions/generateUniqueHandle';
 
 const { User } = db;
 
-const signUp = async (req: Request, res: Response) => {
-    const { email, password, username, handle } = req.body;
-    const errors = validateUserEntry(req.body);
-    if (errors.length > 0) {
-        return apiError(res, 'Validation error', errors, 400);
+const register = async (req: Request, res: Response) => {
+    const { email, password } = req.body;
+    // const errors = validateUserEntry(req.body);
+    // if (errors.length > 0) {
+    //     return apiError(res, 'Validation error', errors, 400);
+    // }
+    const existingEmail = await User.findOne({ where: { email } });
+    if (existingEmail) {
+        return apiError(res, 'Email already in use', 400);
     }
-    if (handle) {
-        const existingHandle = await User.findOne({ where: { handle } });
-        if (existingHandle) {
-          return apiError(res, 'Handle already exists', 400);
-        }
-      }
-      const userHandle = handle || await generateUniqueHandle(username);
+    const username = await generateUsernameFromEmail(email);
+    const handle = await generateUniqueHandle(username);
+
+    const user = await User.create({ email, password, username, handle}); console.log(user);
+    if (!user) {
+        return apiError(res, 'User not created', 400);
+    }
 
     try {
-        const user = await User.create({ email, password, username, handle: userHandle });
-        return apiSuccess(res, 'User created successfully', user, 201);
+        const accessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '24h' });
+        return apiSuccess(res, 'User created successfully', { user, accessToken}, 201);
     } catch (error) {
         return handleControllerError(res, error, 'An error occurred while creating the user.');
     }
 }
 
-const signIn = async (req: Request, res: Response) => {
+const login = async (req: Request, res: Response) => {
     const { email, password } = req.body;
     const errors = validateUserEntry(req.body);
     if (errors.length > 0) {
@@ -47,11 +52,11 @@ const signIn = async (req: Request, res: Response) => {
             return apiError(res, 'Invalid password', 400);
         }
 
-        const token = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '24h' }); console.log(token);
-        return apiSuccess(res, 'User logged in successfully', { user, token });
+        const accessToken = jwt.sign({ id: user.id }, process.env.JWT_SECRET as string, { expiresIn: '24h' });
+        return apiSuccess(res, 'User logged in successfully', { user, accessToken }, 200);
     } catch (error) {
         return handleControllerError(res, error, 'An error occurred while signing in the user.');
     }
 }
 
-export { signUp, signIn };
+export { register, login };
