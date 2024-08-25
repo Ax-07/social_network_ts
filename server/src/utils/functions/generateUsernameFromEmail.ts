17714311889474c31cc71c6731710e7e42597cbe
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-25 22:47:58 +0200

    creation fonction pour generer un username a l'inscription

---
+++ server/src/utils/functions/generateUsernameFromEmail.ts
@@ -0,0 +1,18 @@
+import { User } from "../../models/user.model";
+
+const generateUsernameFromEmail = async (email: string): Promise<string> => {
+  const username = email.split("@")[0];
+  let existingUser = await User.findOne({ where: { username } });
+  let suffix = 1;
+
+  while (existingUser) {
+    existingUser = await User.findOne({
+      where: { username: `${username}${suffix}` },
+    });
+    suffix++;
+  }
+
+  return username;
+};
+
+export default generateUsernameFromEmail;
