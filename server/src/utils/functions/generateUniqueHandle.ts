3cfa21b39c62e1428102aecce9e191edfe5890df
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-25 22:52:37 +0200

    mise en forme

--- server/src/utils/functions/generateUniqueHandle.ts
+++ server/src/utils/functions/generateUniqueHandle.ts
@@ -1,18 +1,18 @@
-import { User } from '../../models/user.model';
+import { User } from "../../models/user.model";
 
 // Générer un handle unique
 const generateUniqueHandle = async (username: string): Promise<string> => {
-    let handle = "@"+username.charAt(0).toUpperCase() + username.slice(1).toLowerCase().replace(/\s+/g, '');
-    let existingUser = await User.findOne({ where: { handle } });
-    let suffix = 1;
-  
-    while (existingUser) {
-      handle = `${"@"+username.charAt(0).toUpperCase() + username.slice(1).toLowerCase().replace(/\s+/g, '')}${suffix}`;
-      existingUser = await User.findOne({ where: { handle } });
-      suffix++;
-    }
-  
-    return handle;
-  };
+  let handle = "@"+username.charAt(0).toUpperCase() + username.slice(1).toLowerCase().replace(/\s+/g, '');
+  let existingUser = await User.findOne({ where: { handle } });
+  let suffix = 1;
+
+  while (existingUser) {
+    handle = `${"@"+username.charAt(0).toUpperCase() + username.slice(1).toLowerCase().replace(/\s+/g, '')}${suffix}`;
+    existingUser = await User.findOne({ where: { handle } });
+    suffix++;
+  }
+
+  return handle;
+};
 
 export default generateUniqueHandle;
