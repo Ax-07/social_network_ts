31495a9649b02118de2968a87186e6d7a40e401b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-26 18:21:41 +0200

    maj pour afficher le nombre de followers

--- client/src/pages/profile/Profile.tsx
+++ client/src/pages/profile/Profile.tsx
@@ -1,12 +1,27 @@
 import TabList from "../../components/tabList/TabList";
-import { useGetUserByIdQuery } from "../../services/api/userApi";
+import { useGetFollowersNamesQuery, useGetUserByIdQuery } from "../../services/api/userApi";
 import { useParams } from "react-router-dom";
 import { Routes, Route } from "react-router-dom";
 import { getFormattedDate } from "../../utils/functions/formatedDate";
+import { useEffect, useState } from "react";
+import { RootState } from "../../services/stores";
+import { useSelector } from "react-redux";
 
 const Profile = () => {
   const params = useParams<{ id: string }>();
   const { data: { data: user } = {} } = useGetUserByIdQuery(params.id || "");
+  const { data: followersData } = useGetFollowersNamesQuery(user?.id ?? '');
+  const [filteredFollowersNames, setFilteredFollowersNames] = useState<string[]>([]);
+  const userName = useSelector((state: RootState) => state?.auth?.user?.username);
+
+  useEffect(() => {
+      if (followersData?.data?.followersNames && userName) {
+          const filteredNames = followersData.data.followersNames.filter(
+              (followerName) => followerName !== userName
+          );
+          setFilteredFollowersNames(filteredNames);
+      }
+  }, [followersData, userName]);
   return (
     <div className="profile">
       <header className="profile__header">
@@ -40,13 +55,16 @@ const Profile = () => {
         </div>
         <div className="profile__info-follow">
             <div className="profile__info-follow-row">
-                <h3 className="fs-20-700">{user?.followings}</h3>
-                <p className="fs-15-600">Abonnements</p>
+                <h3 className="fs-20-700">{user?.followings?.length} Abonnements</h3>
             </div>
             <div className="profile__info-follow-row">
-                <h3 className="fs-20-700">{user?.followers}</h3>
-                <p className="fs-15-600">Abonnés</p>
+                <h3 className="fs-20-700">{user?.followers?.length} Abonnés</h3>
             </div>
+            <p className="fs-15-600">Suivi par {
+              filteredFollowersNames && filteredFollowersNames?.map((followerName, index) => (
+                <span key={index}>{followerName}{index === filteredFollowersNames.length - 1 ? ' ' : ', '}</span>
+              ))
+              } que vous suivez</p>
         </div>
 
       </div>
