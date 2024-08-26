32a8234f83e2d6128b9b5fdd5c2b9482375f3b0e
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-26 18:19:46 +0200

    modif du types de la reponse  a l'authenfication

--- client/src/services/auth/authApi.ts
+++ client/src/services/auth/authApi.ts
@@ -1,28 +1,30 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
-import { User } from "../api/userApi";
+import { User } from "../../utils/types/user.types";
 
 const localUrl = "http://localhost:8080/api";
 
-export interface LoginResponse {
+export interface AuthResponse {
+  status: string;
   data: {
     user: User;
     accessToken: string;
-    refreshToken: string;
+    refreshToken?: string;
   };
+  message: string;
 }
 
 export const authApi = createApi({
   reducerPath: "authApi",
   baseQuery: fetchBaseQuery({ baseUrl: localUrl }),
   endpoints: (builder) => ({
-    login: builder.mutation<LoginResponse, { email: string; password: string }>({
+    login: builder.mutation<AuthResponse, { email: string; password: string }>({
       query: ({ email, password }) => ({
         url: "/auth/login",
         method: "POST",
         body: { email, password },
       }),
     }),
-    register: builder.mutation<User, { email: string; password: string; username: string}>({
+    register: builder.mutation<AuthResponse, { email: string; password: string }>({
       query: (user) => ({
         url: "/auth/register",
         method: "POST",
