7f5885f3c00efeee0215fc6c82d249e8106cd305
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-26 18:22:16 +0200

    maj de l'affichage pour refleter celui de twitter

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,14 +1,22 @@
 import { Routes, Route } from "react-router-dom";
 import PostList from "../../components/post/PostsList";
-import PostPage from "../post/PostPage";
-import CommentPage from "../post/CommentPage";
+import TabList from "../../components/tabList/TabList";
+import { PostFormProvider } from "../../components/post/context/postFormContext";
+import AddPost from "../../components/post/AddPost";
 
 const Home = () => {
   return (
     <section className="home">
+      <TabList
+        links={[
+          { name: "Pour vous", to: "/home/posts" },
+          { name: "Abonnements", to: "/home/abonnements" },
+        ]}
+      />
+      <PostFormProvider origin="page-home">
+        <AddPost origin="page-home" />
+      </PostFormProvider>
       <Routes>
-        <Route path="/posts/:id" element={<PostPage />} />
-        <Route path="/comment/:id" element={<CommentPage />} />
         <Route path="/posts" element={<PostList />} />
         <Route path="/abonnements" element={<h2>Abonnements</h2>} />
         <Route path="/coding-lab" element={<h2>CodingLab</h2>} />
