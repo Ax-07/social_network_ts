27721ca6581d03e016e7f860cd690c87f61a75f4
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-26 18:11:38 +0200

    ajout route pour recuperer les noms des followers

--- server/src/routes/follow.routes.ts
+++ server/src/routes/follow.routes.ts
@@ -1,10 +1,10 @@
 import { Router } from 'express';
-import { followUser, unfollowUser } from '../controllers/follow.controller';
+import { followUser, getFollowersNames } from '../controllers/follow.controller';
 import { checkAuth } from '../middleware/checkAuth.middelware';
 
 const router = Router();
 
 router.patch('/follow', checkAuth, followUser);
-router.patch('/unfollow', checkAuth, unfollowUser);
+router.get('/users/:id/followers', checkAuth, getFollowersNames);
 
 export default router;
