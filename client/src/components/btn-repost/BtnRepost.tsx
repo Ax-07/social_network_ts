d72049b2d76d50f09f44c78e5e30d5eb4640a4ab
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-27 13:43:28 +0200

    ajout setPostId pour le repost

--- client/src/components/btn-repost/BtnRepost.tsx
+++ client/src/components/btn-repost/BtnRepost.tsx
@@ -11,7 +11,7 @@ const BtnRepost: FunctionComponent<BtnRepostProps> = ({ postId }) => {
   const [isOpen, setIsOpen] = useState(false);
   const [modalPosition, setModalPosition] = useState({ top: 0, left: 0 });
   const buttonRef = useRef<HTMLDivElement>(null);
-  const { modals, openModal, closeModal } = useModal();
+  const { modals, openModal, closeModal, setPostId } = useModal();
 
   useEffect(() => {
     if (modals['btn-repost'] && buttonRef.current) {
@@ -39,6 +39,12 @@ const BtnRepost: FunctionComponent<BtnRepostProps> = ({ postId }) => {
     closeModal('btn-repost'); // Fermer la modal
   };
 
+  const handleOpenRepostModal = () => {
+    openModal('modal-repost');
+    setPostId(postId ?? '');
+    handleClose();
+  }
+
   return (
     <>
       <div ref={buttonRef} className="btn-repost__primary" onClick={handleOpen}>
@@ -56,7 +62,7 @@ const BtnRepost: FunctionComponent<BtnRepostProps> = ({ postId }) => {
               <img src="/src/assets/icons/faRetweet.svg" alt="icon retweet" />
               <p>Repost</p>
             </div>
-            <div className="btn-repost__modal-option" onClick={handleClose}>
+            <div className="btn-repost__modal-option" onClick={handleOpenRepostModal}>
               <img src="/src/assets/icons/faPencilAlt.svg" alt="icon pen" />
               <p>Citation</p>
             </div>
