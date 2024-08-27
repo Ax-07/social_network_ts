c75d8fc028e54818f3627b0cf6f510e40981503b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-27 13:58:23 +0200

    ajout originalPostid

--- client/src/utils/types/form.types.ts
+++ client/src/utils/types/form.types.ts
@@ -1,7 +1,8 @@
 export interface FormState {
-    userId: string | undefined;
-    content: string;
-    file: File | string;
+    userId?: string | undefined;
+    content?: string;
+    file?: File | string;
+    originalPostId?: string | undefined;
     commentedPostId?: string;
     commentedCommentId?: string;
   }
