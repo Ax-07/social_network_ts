3d91f9cf332495aa895d1241a40a8196a3e74dfd
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-27 13:47:50 +0200

    creation composant addRepost

---
+++ client/src/components/post/AddRepost.tsx
@@ -0,0 +1,49 @@
+import { useAddPostMutation } from "../../services/api/postApi";
+import { usePushToast } from "../toast/Toasts";
+import { ApiError } from "../../utils/types/api.types";
+import PostForm, { PostFormOrigin } from "./PostForm";
+import { usePostFormContext } from "./hooks/usePostFormContext";
+import { useEffect } from "react";
+
+interface AddPostProps {
+  origin: PostFormOrigin;
+  onClose?: () => void;
+}
+
+const AddRepost = ({ origin, onClose }: AddPostProps) => {
+  const {
+    form,
+    resetForm,
+  } = usePostFormContext();
+
+  const [addPost, { isLoading }] = useAddPostMutation();
+  const pushToast = usePushToast();
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const formData = new FormData();
+      formData.append("userId", form.userId as string);
+      formData.append("content", form.content as string);
+      formData.append("originalPostId", form.originalPostId as string);
+
+      const response = await addPost(formData).unwrap();
+      pushToast({ message: response.message, type: "success" });
+    } catch (error) {
+      pushToast({ message: (error as ApiError).data.message, type: "error" });
+    } finally {
+      resetForm();
+      onClose && onClose();
+    }
+  };
+  
+  useEffect(() => {
+    console.log("AddRepost", origin);
+      console.log("AddRepost", form);
+    }, [form, origin]);
+    
+    if (isLoading) return <p>Publication en cours...</p>;
+  return <PostForm handleSubmit={handleSubmit} origin={origin}/>
+};
+
+export default AddRepost;
