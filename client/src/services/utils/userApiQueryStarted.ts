4f70394f04bcae892f7105470e1bb7462c73984d
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-29 11:46:30 +0200

    ajout gestion su store de bookmark

--- client/src/services/utils/userApiQueryStarted.ts
+++ client/src/services/utils/userApiQueryStarted.ts
@@ -24,3 +24,27 @@ export const updateUserCacheAfterFollow = async (userId: string, followerId: str
         console.error("Failed to update cache after following user:", error);
     }
 }
+
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const updatePostCacheAfterAddToBookmarks = async ({userId}: {userId: string, postId: string}, {dispatch, queryFulfilled}: {dispatch: any, queryFulfilled: any}) => {
+    try {
+        const { data } = await queryFulfilled;
+        console.log('Data from queryFulfilled:', data);
+    
+        dispatch(
+          UserApi.util.updateQueryData("getUserById", userId, (draft) => {
+            const userUpdated = draft.data;
+            if (userUpdated) {
+              console.log('User before update:', JSON.parse(JSON.stringify(userUpdated))); // Objet d'origine
+    
+              // Appliquer la mise à jour
+              userUpdated.bookmarks = data.data.bookmarks;
+    
+              console.log('User after update:', JSON.parse(JSON.stringify(userUpdated))); // Vérifier l'objet après modification
+            }
+          })
+        );
+      } catch (error) {
+        console.error("Failed to update cache after adding post to bookmarks:", error);
+      }
+    };
