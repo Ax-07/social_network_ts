ca99c359cb0e5f9f9d5fa7b2ffa5fc81f11bdb00
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-29 23:51:20 +0200

    refactoring et ajout de jsdoc

--- client/src/components/userProfile/UserProfileThumbnail.tsx
+++ client/src/components/userProfile/UserProfileThumbnail.tsx
@@ -1,26 +1,49 @@
-import { shallowEqual, useDispatch, useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useWindowSize } from "../../utils/hooks/useWindowSize";
 import { RootState } from "../../services/stores";
 import { NavLink } from "react-router-dom";
 import { logout } from "../../services/auth/authSlice";
-import { useState } from "react";
+import { FunctionComponent, useState } from "react";
+import { User } from "../../utils/types/user.types";
 
-export const UserPicture = () => {
-  const user = useSelector((state: RootState) => state.auth?.user, shallowEqual);
+interface IProfilPicture {
+  user?: User | null;
+  withLink?: boolean;
+}
+/**
+ * Composant ProfilPicture
+ * @param {IProfilPicture} props - Les propriétés du composant.
+ * @param {User | null} props.user - L'utilisateur dont la photo de profil doit être affichée. Peut être null ou undefined.
+ * @param {boolean} [props.withLink=true] - Indique si la photo de profil doit être cliquable avec un lien vers le profil de l'utilisateur.
+ * @returns {JSX.Element}
+ * @description Affiche la photo de profil de l'utilisateur avec ou sans lien vers son profil.
+ * 
+ * @see {@link User} pour les informations sur l'utilisateur.
+ * 
+ * @example
+ * // Utilisation avec lien vers le profil (par défaut)
+ * <ProfilPicture user={user} withLink={true} />
+ * 
+ * @example
+ * // Utilisation sans lien vers le profil
+ * <ProfilPicture user={user} withLink={false} />
+ */
+export const ProfilPicture: FunctionComponent<IProfilPicture> = ({ user, withLink = true }) => {
+  const profileImage = (
+    <img
+      className="userprofile__picture"
+      src={user?.profilPicture ? user.profilPicture : "/images/Default-user-picture.png"}
+      alt="User Profile Thumbnail"
+      loading="lazy"
+    />
+  );
 
-  return (
-    <NavLink to={`/profile/${user?.id}`}>
-      <img
-        className="userprofile__picture"
-        src={
-          user?.profilPicture
-            ? user.profilPicture
-            : "/images/Default-user-picture.png"
-        }
-        alt="User Profile Thumbnail"
-        loading="lazy"
-      />
+  return withLink && user?.id ? (
+    <NavLink to={`/profile/${user.id}`}>
+      {profileImage}
     </NavLink>
+  ) : (
+    profileImage
   );
 };
 
@@ -37,11 +60,7 @@ const UserProfileThumbnail = () => {
   return (
     <>
       <div className="userprofile__thumbnail" onClick={() => setIsOpen(!isOpen)}>
-        <img className="userprofile__picture"
-          src={ user?.profilPicture ? user.profilPicture : "/images/Default-user-picture.png" }
-          alt="User Profile Thumbnail"
-          loading="lazy"
-        />
+        <ProfilPicture user={user} withLink={false}/>
         {!isTablet && (
           <>
             <div className="userprofile__thumbnail-info">
