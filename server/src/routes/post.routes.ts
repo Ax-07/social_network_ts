1baddc3936d5f540212030102e8f62c677239d7b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-29 11:47:47 +0200

    ajout route bookmark et modif route view

--- server/src/routes/post.routes.ts
+++ server/src/routes/post.routes.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { createPost, rePost, deletePost, getAllPosts, getPostById, updatePost, viewPost } from "../controllers/post.controller";
+import { createPost, rePost, deletePost, getAllPosts, getPostById, updatePost, viewPost, getBookmarkedPosts } from "../controllers/post.controller";
 import uploadFileMiddleware from "../middleware/multer.middleware";
 import { checkAuth } from "../middleware/checkAuth.middelware";
 
@@ -8,9 +8,10 @@ const router = Router();
 router.post('/posts', checkAuth, uploadFileMiddleware, createPost);
 router.post('/reposts', checkAuth, uploadFileMiddleware, rePost);
 router.get('/posts', getAllPosts);
+router.get('/posts/bookmarks', checkAuth, getBookmarkedPosts);
 router.get('/posts/:id', getPostById);
 router.patch('/posts/:id', checkAuth, uploadFileMiddleware, updatePost);
 router.delete('/posts/:id', checkAuth, deletePost);
-router.patch('/posts/views/:id', checkAuth, viewPost);
+router.patch('/posts/:id/views', checkAuth, viewPost);
 
 export default router;
