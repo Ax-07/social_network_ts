40e5db2b3d9b4ec512962f47ebf501431f454e23
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-31 23:12:32 +0200

    ajout jsdoc

--- server/src/utils/functions/validations/validateCommentEntry.ts
+++ server/src/utils/functions/validations/validateCommentEntry.ts
@@ -3,7 +3,36 @@ import { CommentAttributes } from "../../../models/comment.model";
 
 interface CommentEntry extends Optional<CommentAttributes, "id"> {};
 
-const validateCommentEntry = (entry: CommentEntry) => {
+/**
+ * Valide les données d'entrée pour la création d'un commentaire.
+ * 
+ * Cette fonction vérifie si les champs du commentaire respectent les conditions requises, telles que la présence de chaînes non vides et les formats corrects pour les URLs de média. 
+ * Elle renvoie un tableau d'erreurs si des validations échouent.
+ *
+ * @function validateCommentEntry
+ * @param {CommentEntry} entry - L'objet représentant les attributs du commentaire à valider.
+ * @returns {string[]} Un tableau contenant les messages d'erreur de validation. Le tableau est vide si toutes les validations passent.
+ * 
+ * @property {string} [entry.postId] - L'ID du post auquel le commentaire est lié. Doit être une chaîne non vide si présent.
+ * @property {string} [entry.commentId] - L'ID du commentaire auquel ce commentaire est une réponse. Doit être une chaîne non vide si présent.
+ * @property {string} [entry.userId] - L'ID de l'utilisateur créant le commentaire. Doit être une chaîne non vide.
+ * @property {string} [entry.content] - Le contenu du commentaire. Doit être une chaîne non vide.
+ * @property {string} [entry.media] - L'URL du média associé au commentaire. Si présent, doit être une URL valide.
+ * @property {string} [entry.commentedPostId] - L'ID du post commenté en cas de repost. Doit être une chaîne non vide si présent.
+ * @property {string} [entry.commentedCommentId] - L'ID du commentaire parent en cas de réponse à un commentaire. Doit être une chaîne non vide si présent.
+ * 
+ * @example
+ * const errors = validateCommentEntry({
+ *   postId: "123",
+ *   userId: "456",
+ *   content: "This is a comment",
+ *   media: "http://example.com/media.jpg"
+ * });
+ * if (errors.length > 0) {
+ *   console.log("Validation errors:", errors);
+ * }
+ */
+const validateCommentEntry = (entry: CommentEntry): string[] => {
     const errors: string[] = [];
 
     if ((entry.postId && !entry.commentId) &&  (typeof entry.postId !== 'string' || entry.postId.length === 0)) {
