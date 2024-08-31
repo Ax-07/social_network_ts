1783f97295e1a0bb0722a347c141ba78bdbb83da
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-31 23:04:09 +0200

    creation de la page des notification

--- client/src/pages/notifications/Notifications.tsx
+++ client/src/pages/notifications/Notifications.tsx
@@ -1,9 +1,61 @@
-const Notifications = () => {
-    return (
-        <div>
-            <h1>Notifications</h1>            
-        </div>
-    );
+import React, { useEffect, useState } from 'react';
+import { useSelector } from 'react-redux';
+import { RootState } from '../../services/stores';
+import { useGetAllNotificationsByUserIdQuery } from '../../services/api/notificationApi';
+import { NotificationTypes } from '../../utils/types/notification.types';
+import TabList from '../../components/tabList/TabList';
+import { Routes, Route } from 'react-router-dom';
+import NotificationsList from './NotificationsList';
+
+const Notifications: React.FC = () => {
+  const userId = useSelector((state: RootState) => state.auth.user?.id);
+  const notificationsFromStore = useSelector((state: RootState) => state.notifications.notifications);
+  const {data: {data: notificationFromApi} = {}} = useGetAllNotificationsByUserIdQuery(userId as string);
+  const [uniqueNotifications, setUniqueNotifications] = useState<NotificationTypes[]>([]);
+  
+  useEffect(() => {
+    if (notificationFromApi) {
+      const mergedNotifications = [...notificationsFromStore]; console.log('mergedNotifications', mergedNotifications);
+
+      notificationFromApi.forEach((notif) => {
+        const existsInStore = notificationsFromStore.some((notifStore) => notif.id === notifStore.id);
+        if (!existsInStore) {
+          mergedNotifications.push(notif);
+        }
+      });
+
+      const sortedNotifications = mergedNotifications.sort((a, b) => {
+        return new Date(b.createdAt as string).getTime() - new Date(a.createdAt as string).getTime();
+      });
+
+      setUniqueNotifications(sortedNotifications);
+    }
+  }, [notificationFromApi, notificationsFromStore]);
+
+
+  return (
+    <div className='notification-page'>
+      <h2 className='notification-page__title'>Notifications</h2>
+      <TabList 
+        links={[
+          {name: 'Tous', to: '/notifications', end: true},
+          // {name: 'Certifié', to: '/notifications/verified'},
+          {name: 'Mentions', to: '/notifications/mentions', end: false}
+        ]}
+        />
+        <Routes>
+          <Route path='/' element={<NotificationsList notifications={uniqueNotifications}/>} />
+          <Route path='/mentions' element={<h2>Mentions</h2>} />
+        </Routes>
+      {/* <ul className='notification-page__list'>
+        {uniqueNotifications.map((notif, index) => (
+          <li key={index} className='notification-page__item'>
+            <NotificationCard {...notif} />
+          </li>
+        ))}
+      </ul> */}
+    </div>
+  );
 };
 
 export default Notifications;
