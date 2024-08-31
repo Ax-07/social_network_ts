481919f8cc3cd0638d563885e4b9a86a6cda8ef5
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-08-31 23:06:18 +0200

    creation notification api

---
+++ client/src/services/api/notificationApi.ts
@@ -0,0 +1,38 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import { RootState } from "../stores";
+import { NotificationsResponseArray } from "../../utils/types/notification.types";
+
+const localUrl = "http://localhost:8080/api";
+
+export const notificationApi = createApi({
+  reducerPath: "notificationApi",
+  baseQuery: fetchBaseQuery({
+    baseUrl: localUrl,
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.accessToken;
+      if (token) {
+        headers.set("authorization", `Bearer ${token}`);
+      }
+      return headers;
+    },
+  }),
+  tagTypes: ["Notifications"],
+  endpoints: (builder) => ({
+    getAllNotificationsByUserId: builder.query<NotificationsResponseArray, string>({
+      query: (userId) => `/notifications/${userId}`,
+      providesTags: ["Notifications"],
+    }),
+    removeNotification: builder.mutation<string, string>({
+      query: (id) => ({
+        url: `/notifications/${id}`,
+        method: "DELETE",
+      }),
+      invalidatesTags: ["Notifications"],
+    }),
+  }),
+});
+
+export const {
+  useGetAllNotificationsByUserIdQuery,
+  useRemoveNotificationMutation,
+} = notificationApi;
