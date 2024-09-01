f2084ea615d58a348ca041a55722552b821d706a
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-01 22:49:44 +0200

    refactoring

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,50 +1,41 @@
 import express, { Request, Response } from "express";
 import http from "http";
 import { Server } from 'socket.io';
-import cors from "cors";
 import * as dotenv from "dotenv";
 import path from "path";
-import userRoutes from "./routes/user.routes";
-import postRoutes from "./routes/post.routes";
-import commentRoutes from "./routes/comment.routes";
-import likeRoutes from "./routes/likes.routes";
-import msgRoutes from "./routes/messenging.routes";
-import authRoutes from "./routes/auth.routes";
-import followRoutes from "./routes/follow.routes";
-import notificationRoutes from "./routes/notification.routes";
+import routes from "./routes/index.routes";
 import db from "./models";
 import { configureSocket } from "./services/webSocket";
 import serverError from "./utils/errors/server.error";
 import { initializeNotificationWebSocket } from './services/notifications';
+import { applyServerSecurity } from "./utils/serverSecurity";
 
 dotenv.config();
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
+// Configuration du serveur WebSocket
 configureSocket(io);
 
-app.use(cors());
-app.use(express.json({limit: '10kb'}));
-app.use(express.urlencoded({ extended: true }));
+// Middleware pour la sécurité du serveur
+applyServerSecurity(app);
+
+// Routes statiques pour les images et les vidéos
 app.use('/images', express.static(path.join(__dirname, '..', 'public', 'images')));
 app.use('/videos', express.static(path.join(__dirname, '..', 'public', 'videos')));
 
+// route pour verifier que le serveur fonctionne
 app.get("/", (req: Request, res: Response) => {
   res.send("Hello World!");
 });
+
+// route pour tester la messagerie instantanée
 app.get("/socket", (req: Request, res: Response) => {
   res.sendFile(path.join(__dirname, '..','src', 'pages', 'socket.html'));
 });
 
-app.use("/api/auth", authRoutes);
-app.use("/api", userRoutes);
-app.use("/api", postRoutes);
-app.use("/api", commentRoutes);
-app.use('/api', likeRoutes);
-app.use('/api', msgRoutes);
-app.use('/api', followRoutes);
-app.use('/api', notificationRoutes);
+app.use('/api', routes);
 
 app.use(serverError);
 
