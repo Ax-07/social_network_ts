f783a0a2968f0573a0557534fe325b3461c32844
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-02 23:19:43 +0200

    ajout originalCommentId aux types

--- client/src/utils/types/form.types.ts
+++ client/src/utils/types/form.types.ts
@@ -3,6 +3,7 @@ export interface FormState {
     content?: string;
     file?: File | string;
     originalPostId?: string | undefined;
+    originalCommentId?: string | undefined;
     commentedPostId?: string;
     commentedCommentId?: string;
   }
