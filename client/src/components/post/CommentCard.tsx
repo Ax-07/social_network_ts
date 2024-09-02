0a02c2320c50e638547e4a901026293f0fa7d73f
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-02 23:09:18 +0200

    ajout BtnViews et BtnBookmarks

--- client/src/components/post/CommentCard.tsx
+++ client/src/components/post/CommentCard.tsx
@@ -6,63 +6,60 @@ import BtnLike from '../btn-like/BtnLike';
 import BtnRepost from '../btn-repost/BtnRepost';
 import BtnComment from '../btn-comment/BtnComment';
 import { PostFormProvider } from './context/postFormContext';
-import {UserNameHoverDisplayCard, UserThumbnailHoverDisplayCard} from '../userProfile/UserHoverDisplayCard ';
+import BtnViews from '../btn-views/BtnViews';
+import BtnBookmarks from '../btn-bookmarks/BtnBookmarks';
+import { UserThumbnailHoverDisplayCard, UserNameHoverDisplayCard } from '../userProfile/UserHoverDisplayCard ';
 
 interface CommentCardProps {
     comment: CommentTypes;
     origin: string;
 }
 
-const CommentCard: FunctionComponent<CommentCardProps> = ({comment, origin}) => {
+const CommentCard: FunctionComponent<CommentCardProps> = ({ comment, origin }) => {
     const commenterId = comment.userId;
     const commentsCount = comment.commentsCount;
     const { data: { data: commenter } = {} } = useGetUserByIdQuery(commenterId);
     const isWebp = comment.media?.endsWith(".webp");
     const isMp4 = comment.media?.endsWith(".mp4");
     const isYoutubeVideo = comment.media?.includes("youtube.com");
-    
-  return (
-    <>
-      {origin != "comment-page" && <NavLink to={`/home/comment/${comment.id}`} className="post-card__link"></NavLink>}
-      <article className="post-card">
-        <UserThumbnailHoverDisplayCard user={commenter} />
-        <div className="post-card__wrapper">
-          <UserNameHoverDisplayCard user={commenter} createdAt={comment.createdAt} />
-          <p className="post-card__content">{comment.content}</p>
-          {comment.media && isWebp && (
-            <img className="post-card__img"
-              src={comment.media}
-              alt={""}
-              loading="lazy"
-            />
-          )}
-          {comment.media && isMp4 && (
-            <video className="post-card__video" src={comment.media} controls />
-          )}
-          {comment.media && isYoutubeVideo && (
-            <iframe
-            style={{ width: "100%", aspectRatio: "16/9" }}
-              className="post-card__youtube"
-              src={comment.media}
-              title="YouTube video player"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          )}
-          <div className="post-card__footer">
-            <BtnComment 
-              commentId={comment.id} 
-              commentsCount={commentsCount}
-            />
-            <PostFormProvider origin='modal-repost-comment'> 
-              <BtnRepost commentId={comment.id}/>
-            </PostFormProvider>
-            <BtnLike comment={comment} />
-          </div>
-        </div>
-      </article>
-    </>
-  );
+
+    return (
+        <>
+            {origin !== "comment-page" && <NavLink to={`/home/comment/${comment.id}`} className="post-card__link"></NavLink>}
+            <article className="post-card">
+                <UserThumbnailHoverDisplayCard user={commenter} />
+                <div className="post-card__wrapper">
+                    <UserNameHoverDisplayCard user={commenter} createdAt={comment.createdAt} />
+                    <p className="post-card__content">{comment.content}</p>
+                    {comment.media && isWebp && (
+                        <img className="post-card__img" src={comment.media} alt={""} loading="lazy" />
+                    )}
+                    {comment.media && isMp4 && (
+                        <video className="post-card__video" src={comment.media} controls />
+                    )}
+                    {comment.media && isYoutubeVideo && (
+                        <iframe
+                            style={{ width: "100%", aspectRatio: "16/9" }}
+                            className="post-card__youtube"
+                            src={comment.media}
+                            title="YouTube video player"
+                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                            allowFullScreen
+                        ></iframe>
+                    )}
+                    {origin !== 'repost' && <div className="post-card__footer">
+                        <BtnComment commentId={comment.id} commentsCount={commentsCount} />
+                        <PostFormProvider origin='modal-repost-comment'> 
+                            <BtnRepost postId={comment.postId} commentId={comment.id} reposterCount={comment.reposters?.length}/>
+                        </PostFormProvider>
+                        <BtnLike comment={comment} />
+                        <BtnViews viewsCount={comment.views} />
+                        <BtnBookmarks postId={comment.id} userId={commenterId ?? ""}/>
+                    </div>}
+                </div>
+            </article>
+        </>
+    );
 };
 
 export default CommentCard;
