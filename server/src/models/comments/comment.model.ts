78d5e4776ae52780a125533d2629d076b92b6063
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-03 23:03:57 +0200

    maj du controller bookmark pour respecter le model

---
+++ server/src/models/comments/comment.model.ts
@@ -0,0 +1,110 @@
+import { Sequelize, DataTypes, Model, Optional } from "sequelize";
+
+interface CommentAttributes {
+  id: number;
+  userId: string;
+  content: string;
+  media?: string;
+  commentsCount?: number;
+  postId?: string;
+  commentId?: string;
+  commentedPostId?: string;
+  commentedCommentId?: string;
+  views?: number;
+}
+
+interface CommentCreationAttributes extends Optional<CommentAttributes, "id"> {}
+
+class Comment extends Model<CommentAttributes, CommentCreationAttributes> implements CommentAttributes {
+  public id!: number;
+  public userId!: string;
+  public content!: string;
+  public media!: string;
+  public commentsCount?: number;
+  public postId!: string;
+  public commentId!: string;
+  public commentedPostId!: string;
+  public commentedCommentId!: string;
+  public views!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+const initializeCommentModel = (sequelize: Sequelize): typeof Comment => {
+  Comment.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        references: {
+          model: "users",
+          key: "id",
+        },
+      },
+      content: {
+        type: DataTypes.TEXT,
+        allowNull: false,
+      },
+      media: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      commentsCount: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+      postId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        references: {
+          model: "posts",
+          key: "id",
+        },
+      },
+      commentId: {
+        type: DataTypes.STRING,
+        allowNull: true,
+        references: {
+          model: "comments",
+          key: "id",
+        },
+      },
+      commentedPostId: {
+        type: DataTypes.UUID,
+        allowNull: true,
+        references: {
+          model: "posts",
+          key: "id",
+        },
+      },
+      commentedCommentId: {
+        type: DataTypes.UUID,
+        allowNull: true,
+        references: {
+          model: "comments",
+          key: "id",
+        },
+      },
+      views: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0,
+      },
+    },
+    {
+      tableName: "comments",
+      sequelize,
+    }
+  );
+
+  return Comment;
+};
+
+export { Comment, CommentAttributes, initializeCommentModel };
