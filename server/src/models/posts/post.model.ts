3128380418f2863f79acbf09159ff3500eb5d4db
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-03 23:08:11 +0200

    creation model post

---
+++ server/src/models/posts/post.model.ts
@@ -0,0 +1,80 @@
+import { Sequelize, DataTypes, Model, Optional } from 'sequelize';
+
+interface PostAttributes {
+  id: string;
+  userId: string;
+  content?: string;
+  commentsCount?: number;
+  media?: string | null;
+  originalPostId?: string | null;
+  views?: number;
+}
+
+interface PostCreationAttributes extends Optional<PostAttributes, 'id'> {}
+
+class Post extends Model<PostAttributes, PostCreationAttributes> implements PostAttributes {
+  public id!: string;
+  public userId!: string;
+  public content!: string;
+  public media!: string | null;
+  public commentsCount!: number;
+  public originalPostId!: string | null;
+  public views!: number;
+
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
+}
+
+const initializePostModel = (sequelize: Sequelize): typeof Post => {
+  Post.init(
+    {
+      id: {
+        type: DataTypes.UUID,
+        defaultValue: DataTypes.UUIDV4,
+        primaryKey: true,
+      },
+      userId: {
+        type: DataTypes.UUID,
+        allowNull: false,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      content: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      media: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
+      commentsCount: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      },
+      originalPostId: {
+        type: DataTypes.UUID,
+        allowNull: true,
+        references: {
+          model: 'posts',
+          key: 'id',
+        },
+      },
+      views: {
+        type: DataTypes.INTEGER,
+        allowNull: true,
+        defaultValue: 0
+      }
+    },
+    {
+      tableName: 'posts',
+      sequelize,
+    }
+  );
+
+  return Post;
+};
+
+export { Post, PostAttributes, initializePostModel };
