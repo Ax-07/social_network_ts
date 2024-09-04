694f614567a29b86a55bdeefb6a926a3ceb885fd
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-04 23:24:50 +0200

    modif des types en fonction des nouveaux models

--- client/src/utils/types/user.types.ts
+++ client/src/utils/types/user.types.ts
@@ -9,11 +9,31 @@ export interface User {
     coverPicture?: string; // Image de couverture
     bio?: string; // Biographie
     birthdate?: Date; // Date de naissance
-    followers?: string[]; // Liste des abonnés
-    followings?: string[]; // Liste des abonnements
-    bookmarks?: string[]; // Liste des signets
+    followers?: {id: string, username: string, profilPicture: string}[]; // Liste des abonnés
+    followings?: {id: string, username: string, profilPicture: string}[]; // Liste des abonnements
+    bookmarks?: {postId: string, userId: string}[]; // Liste des signets (id est l'id du post)
     notifications?: { type: string; message: string; postId: string }[]; // Liste des notifications
     createdAt?: Date; // Date de création
+}
+
+export interface UserEntry {
+    username: string; // Nom d'utilisateur
+    email: string; // Adresse e-mail
+    password: string; // Mot de passe haché
+    profilPicture?: string; // Image de profil
+    coverPicture?: string; // Image de couverture
+    bio?: string; // Biographie
+    birthdate?: Date; // Date de naissance
+}
+
+export interface UserFollow {
+    followerId: string; // Identifiant de l'utilisateur qui suit
+    followedId: string; // Identifiant de l'utilisateur suivi
+}
+
+export interface UserBookmarks {
+    userId: string; // Identifiant de l'utilisateur
+    postId: string; // Identifiant de la publication
 }
 
 export interface UserResponseArray {
