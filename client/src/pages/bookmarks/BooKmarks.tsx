a88dbcdf5bca7d742c4bfe8d7572d6d382284884
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-04 23:23:32 +0200

    ajout refetch

--- client/src/pages/bookmarks/BooKmarks.tsx
+++ client/src/pages/bookmarks/BooKmarks.tsx
@@ -2,13 +2,19 @@ import { useSelector } from "react-redux";
 import { useGetBookmarkedPostsQuery } from "../../services/api/postApi";
 import { RootState } from "../../services/stores";
 import PostCard from "../../components/post/PostCard";
+import { useEffect } from "react";
 
 const BooKmarks = () => {
   const userId = useSelector((state: RootState) => state.auth.user?.id);
   const userHandle = useSelector((state: RootState) => state.auth.user?.handle);
-  const { data: { data: postsBookmarked } = {} } = useGetBookmarkedPostsQuery(
+  const { data: { data: postsBookmarked } = {}, refetch } = useGetBookmarkedPostsQuery(
     userId as string
   );
+
+  useEffect(() => {
+    refetch();
+  }, []);
+
   return (
     <div className="bookmarks-page">
       <div className="bookmarks-page__header">
