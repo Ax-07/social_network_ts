8981baef1d6a6cabd83ea3ffaa417a8661f47347
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-05 22:24:02 +0200

    ajout fonction handleclick pour éviter l'exécution si le bouton est désactivé

--- client/src/components/button/Button.tsx
+++ client/src/components/button/Button.tsx
@@ -8,12 +8,24 @@ interface ButtonProps extends PropsWithChildren<{
 }> {}
 
 const Button: FunctionComponent<ButtonProps> = (props) => {
+    // Gestion de l'événement onClick pour éviter l'exécution si le bouton est désactivé
+    const handleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
+      if (props.disabled) {
+        e.preventDefault();
+        return;
+      }
+      if (props.onClick) {
+        props.onClick();
+      }
+    };
   return (
     <button
-      onClick={props.onClick}
+      onClick={handleClick}
       type={props.type}
       className={`${props.className}`}
       disabled={props.disabled}
+      aria-disabled={props.disabled} // Ajout d'aria-disabled pour l'accessibilité
+
     >
       {props.children}
     </button>
