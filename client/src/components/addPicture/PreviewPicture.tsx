12fc466f9cb4b97b0ec50db5afca5e9abc394a8b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-05 22:19:40 +0200

    refacto, modif semantique et ajout aria-label

--- client/src/components/addPicture/PreviewPicture.tsx
+++ client/src/components/addPicture/PreviewPicture.tsx
@@ -1,4 +1,4 @@
-import type { FunctionComponent } from 'react';
+import type { FunctionComponent } from "react";
 
 interface PreviewPictureProps {
   media: string;
@@ -6,28 +6,43 @@ interface PreviewPictureProps {
   onCancel: () => void;
 }
 
-const PreviewPicture: FunctionComponent<PreviewPictureProps> = ({ media, mimetype, onCancel }) => {
-  const isPicture = mimetype === 'image/webp' || mimetype === 'image/png' || mimetype === 'image/jpeg';
-  const isVideo = mimetype === 'video/mp4';
-  const isYoutubeVideo = mimetype === 'video/youtube';
+const PreviewPicture: FunctionComponent<PreviewPictureProps> = ({
+  media,
+  mimetype,
+  onCancel,
+}) => {
+  const isPicture =
+    mimetype === "image/webp" ||
+    mimetype === "image/png" ||
+    mimetype === "image/jpeg";
+  const isVideo = mimetype === "video/mp4";
+  const isYoutubeVideo = mimetype === "video/youtube";
   return (
-    <div className='preview-media'>
-      {isPicture && <img src={media} alt="Preview" />}
-      {isVideo && <video src={media} controls />}
+    <figure className="preview-media">
+      {isPicture && <img src={media} alt="Aperçu de l'image sélectionnée" />}
+      {isVideo && (
+        <video
+          src={media}
+          controls
+          aria-label="Aperçu de la vidéo sélectionnée"
+        />
+      )}
       {isYoutubeVideo && (
         <iframe
           width="100%"
           height="315"
           src={media}
           title="YouTube video player"
+          aria-label="Lecteur vidéo YouTube"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
           allowFullScreen
         ></iframe>
       )}
-      <button className='btn-close-top-right' type="button" onClick={onCancel}>✖️</button>
-    </div>
+      <button className="btn-close-top-right" type="button" onClick={onCancel} aria-label="Fermer l'aperçu">
+        ✖️
+      </button>
+    </figure>
   );
 };
 
 export default PreviewPicture;
-
