3fdf71383844613fb682c66d88fd6cfdb93e874c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-06 10:05:46 +0200

    ajout des role et des arias

--- client/src/components/tabList/TabList.tsx
+++ client/src/components/tabList/TabList.tsx
@@ -7,10 +7,16 @@ interface TabListProps {
 
 const TabList: FunctionComponent<TabListProps> = (props) => {
   return (
-    <menu className="tablist">
-      {props.links.map((link) => (
+    <menu className="tablist" role="tablist">
+      {props.links.map((link, index) => (
         <div key={link.name}  className="tablist__item">
-        <NavLink to={link.to} end={link.end} className="tablist__link">
+        <NavLink to={link.to} end={link.end} 
+          className="tablist__link"
+          role="tab" // Définit le rôle de l'élément pour l'accessibilité (navigation)
+          aria-selected="true" // L'onglet actif est sélectionné
+          tabIndex={index === 0 ? 0 : -1} // L'onglet actif ou le premier doit être dans le focus au début
+          aria-controls={`tabpanel-${index}`} // Lien vers le panneau de contenu correspondant
+          >
           {link.name}
         </NavLink>
         <hr className="tablist__link--active"/>
