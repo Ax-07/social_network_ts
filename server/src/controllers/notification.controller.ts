14d6d7f90253b85b3e2eae114bfea8016f7087a9
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-06 10:12:23 +0200

    ajout fonction pour marquer les notifications comme lues

--- server/src/controllers/notification.controller.ts
+++ server/src/controllers/notification.controller.ts
@@ -16,4 +16,27 @@ const getAllNotificationsByUserId = async (req: Request, res: Response) => {
     }
 };
 
-export { getAllNotificationsByUserId };
+const markNotificationsAsRead = async (req: Request, res: Response) => {
+    const {notificationsIds} = req.body;
+    const userId = req.params.id;
+    if (!notificationsIds) {
+        return apiError(res, 'Notifications IDs are required', 400);
+    }
+    try {
+        // marque les notifications comme lues
+        const notifications = await db.Notification.findAll({ where: { userId } });
+        if (!notifications) {
+            return apiError(res, 'Notifications not found', 404);
+        }
+        notifications.forEach(async (notification) => {
+            if (notificationsIds.includes(notification.id)) {
+                await notification.update({ isRead: true });
+            }
+        });
+        return apiSuccess(res, 'Notifications marked as read', notifications);
+    } catch (error) {
+        return handleControllerError(res, error, 'An error occurred while marking notifications as read.');
+    }
+}
+
+export { getAllNotificationsByUserId, markNotificationsAsRead };
