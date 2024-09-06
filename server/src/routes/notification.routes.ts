7ad99236a0995c87a645e662590384347236a2a8
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-06 10:13:31 +0200

    ajout de la route pour marquer les notif comme vue

--- server/src/routes/notification.routes.ts
+++ server/src/routes/notification.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { getAllNotificationsByUserId } from "../controllers/notification.controller";
+import { getAllNotificationsByUserId, markNotificationsAsRead } from "../controllers/notification.controller";
 import { checkAuth } from "../middleware/checkAuth.middelware";
 
 const router = Router();
 
 router.get('/notifications/:id', checkAuth, getAllNotificationsByUserId);
+router.patch('/notifications/:id/markAsRead', checkAuth, markNotificationsAsRead);
 
 export default router;
