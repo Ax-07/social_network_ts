32efe3e09a87a2b86171d1141f20fa563660f898
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-10 22:55:58 +0200

    remplacement du context react par celui de redux

--- client/src/pages/home/Home.tsx
+++ client/src/pages/home/Home.tsx
@@ -1,7 +1,6 @@
 import { Routes, Route } from "react-router-dom";
 import PostList from "../../components/Display/post/PostsList";
 import TabList from "../../components/Base/tabList/TabList";
-import { PostFormProvider } from "../../components/Form/context/postFormContext";
 import AddPost from "../../components/Form/AddPost";
 
 const Home = () => {
@@ -13,9 +12,7 @@ const Home = () => {
           { name: "Abonnements", to: "/home/abonnements", end: false },
         ]}
       />
-      <PostFormProvider origin="page-home">
-        <AddPost origin="page-home" />
-      </PostFormProvider>
+      <AddPost origin="page-home" />
       <Routes>
         <Route path="/" element={<PostList />} />
         <Route path="/abonnements" element={<h2>Abonnements</h2>} />
