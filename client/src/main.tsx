607a61c3fa271019b3b5859b731e71470f896884
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-10 23:02:21 +0200

    remplacement du context react par celui de redux

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -5,22 +5,19 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./services/stores";
 import "./scss/index.css";
-import { ModalProvider } from "./components/modal/context/ModalContext.tsx";
 import { ToastsProvider } from "./components/toast/ToastContext.tsx";
 import { StrictMode } from "react";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Provider store={store}>
-        <ModalProvider>
-          <PersistGate loading={null} persistor={persistor}>
-            <ToastsProvider>
-              <BrowserRouter>
-                <App />
-              </BrowserRouter>
-            </ToastsProvider>
-          </PersistGate>
-        </ModalProvider>
+      <PersistGate loading={null} persistor={persistor}>
+        <ToastsProvider>
+          <BrowserRouter>
+            <App />
+          </BrowserRouter>
+        </ToastsProvider>
+      </PersistGate>
     </Provider>
   </StrictMode>
 );
