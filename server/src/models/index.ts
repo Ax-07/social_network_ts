66687f8a7dea42c3042116ca9e561338e28ecd26
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-10 23:08:10 +0200

    ajout commentaire pour les associations

--- server/src/models/index.ts
+++ server/src/models/index.ts
@@ -52,43 +52,48 @@ const db: Database = {
 // foreignKey est utilisé pour spécifier le nom de la clé étrangère dans la base de données
 // through est utilisé pour spécifier le nom de la table de jonction dans les associations many-to-many
 // as est utilisé pour spécifier le nom de l'association dans les requêtes 
-db.User.hasMany(db.Post, { foreignKey: 'userId', as: 'posts' });
-db.Post.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+db.User.hasMany(db.Post, { foreignKey: 'userId', as: 'posts' }); // Un utilisateur a plusieurs posts
+db.Post.belongsTo(db.User, { foreignKey: 'userId', as: 'user' }); // Un post appartient à un utilisateur
 
-db.User.hasMany(db.Comment, { foreignKey: 'userId', as: 'comments' });
-db.Comment.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+db.User.hasMany(db.Comment, { foreignKey: 'userId', as: 'comments' }); // Un utilisateur a plusieurs commentaires
+db.Comment.belongsTo(db.User, { foreignKey: 'userId', as: 'user' }); // Un commentaire appartient à un utilisateur
 
-db.Post.hasMany(db.Comment, { foreignKey: 'postId', as: 'comments' });
-db.Comment.belongsTo(db.Post, { foreignKey: 'postId', as: 'post' });
+db.Post.hasMany(db.Comment, { foreignKey: 'postId', as: 'comments' }); // Un post a plusieurs commentaires
+db.Comment.belongsTo(db.Post, { foreignKey: 'postId', as: 'post' }); // Un commentaire appartient à un post
 
-db.User.belongsToMany(db.User, { through: UserFollowers, as: 'followers', foreignKey: 'followedId', otherKey: 'followerId' });
-db.User.belongsToMany(db.User, { through: UserFollowers, as: 'followings', foreignKey: 'followerId', otherKey: 'followedId' });
+db.User.belongsToMany(db.User, { through: UserFollowers, as: 'followers', foreignKey: 'followedId', otherKey: 'followerId' }); // Un utilisateur a plusieurs followers
+db.User.belongsToMany(db.User, { through: UserFollowers, as: 'followings', foreignKey: 'followerId', otherKey: 'followedId' }); // Un utilisateur suit plusieurs autres utilisateurs
 
-db.UserFollowers.belongsTo(db.User, { foreignKey: 'followerId', as: 'follower' });
-db.UserFollowers.belongsTo(db.User, { foreignKey: 'followedId', as: 'followed' });
+db.UserFollowers.belongsTo(db.User, { foreignKey: 'followerId', as: 'follower' }); // Un follower appartient à un utilisateur
+db.UserFollowers.belongsTo(db.User, { foreignKey: 'followedId', as: 'followed' }); // Un utilisateur suivi appartient à un utilisateur
 
 // Association entre User et Post via UserBookmarks (table de jonction)
-db.User.belongsToMany(db.Post, { through: db.UserBookmarks, as: 'bookmarks', foreignKey: 'userId' });
-db.Post.belongsToMany(db.User, { through: db.UserBookmarks, as: 'bookmarkedBy', foreignKey: 'postId' });
+db.User.belongsToMany(db.Post, { through: db.UserBookmarks, as: 'bookmarks', foreignKey: 'userId' }); // Un utilisateur a plusieurs posts bookmarkés
+db.Post.belongsToMany(db.User, { through: db.UserBookmarks, as: 'bookmarkedBy', foreignKey: 'postId' }); // Un post est bookmarké par plusieurs utilisateurs
 
-db.User.belongsToMany(db.Post, { through: PostLike, as: 'likedPosts', foreignKey: 'userId' });
-db.Post.belongsToMany(db.User, { through: PostLike, as: 'likers', foreignKey: 'postId' });
+// Association entre User et Post via PostLike (table de jonction)
+db.User.belongsToMany(db.Post, { through: PostLike, as: 'likedPosts', foreignKey: 'userId' }); // Un utilisateur a plusieurs posts likés
+db.Post.belongsToMany(db.User, { through: PostLike, as: 'likers', foreignKey: 'postId' }); // Un post est liké par plusieurs utilisateurs
 
-db.User.belongsToMany(db.Post, { through: PostRepost, as: 'repostedPosts', foreignKey: 'userId' });
-db.Post.belongsToMany(db.User, { through: PostRepost, as: 'reposters', foreignKey: 'postId' });
+// Association entre User et Post via PostRepost (table de jonction)
+db.User.belongsToMany(db.Post, { through: PostRepost, as: 'repostedPosts', foreignKey: 'userId' }); // Un utilisateur a plusieurs posts repostés
+db.Post.belongsToMany(db.User, { through: PostRepost, as: 'reposters', foreignKey: 'postId' }); // Un post est reposté par plusieurs utilisateurs
 
-db.User.belongsToMany(db.Comment, { through: CommentLike, as: 'likedComments', foreignKey: 'userId' });
-db.Comment.belongsToMany(db.User, { through: CommentLike, as: 'commentLikers', foreignKey: 'commentId' });
+// Association entre User et Comment via CommentLike (table de jonction)
+db.User.belongsToMany(db.Comment, { through: CommentLike, as: 'likedComments', foreignKey: 'userId' }); // Un utilisateur a plusieurs commentaires likés
+db.Comment.belongsToMany(db.User, { through: CommentLike, as: 'commentLikers', foreignKey: 'commentId' }); // Un commentaire est liké par plusieurs utilisateurs
 
-db.User.belongsToMany(db.Comment, { through: CommentRepost, as: 'repostedComments', foreignKey: 'userId' });
-db.Comment.belongsToMany(db.User, { through: CommentRepost, as: 'commentReposters', foreignKey: 'commentId' });
+// Association entre User et Comment via CommentRepost (table de jonction)
+db.User.belongsToMany(db.Comment, { through: CommentRepost, as: 'repostedComments', foreignKey: 'userId' }); // Un utilisateur a plusieurs commentaires repostés
+db.Comment.belongsToMany(db.User, { through: CommentRepost, as: 'commentReposters', foreignKey: 'commentId' }); // Un commentaire est reposté par plusieurs utilisateurs
 
-db.User.hasMany(db.Notification, { foreignKey: 'userId', as: 'userNotifications' });
-db.Notification.belongsTo(db.User, { foreignKey: 'userId', as: 'user' });
+// Association entre User et Notification
+db.User.hasMany(db.Notification, { foreignKey: 'userId', as: 'userNotifications' }); // Un utilisateur a plusieurs notifications
+db.Notification.belongsTo(db.User, { foreignKey: 'userId', as: 'user' }); // Une notification appartient à un utilisateur
 
 // Association entre User et Group via une table de jonction UserGroups
-db.User.belongsToMany(db.Group, { through: 'UserGroups', as: 'userGroups', foreignKey: 'userId' });
-db.Group.belongsToMany(db.User, { through: 'UserGroups', as: 'groupMembers', foreignKey: 'groupId' });
+db.User.belongsToMany(db.Group, { through: 'UserGroups', as: 'userGroups', foreignKey: 'userId' }); // Un utilisateur appartient à plusieurs groupes
+db.Group.belongsToMany(db.User, { through: 'UserGroups', as: 'groupMembers', foreignKey: 'groupId' }); // Un groupe a plusieurs membres
 
 
 db.Sequelize = Sequelize;
