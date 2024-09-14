b5a9dfdbb2c140c796436215860b11ff8f64ea57
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-14 22:22:14 +0200

    delete route bookmarks

--- server/src/routes/post.routes.ts
+++ server/src/routes/post.routes.ts
@@ -2,14 +2,12 @@ import { Router } from "express";
 import { createPost, rePost, deletePost, getAllPosts, getPostById, updatePost, viewPost } from "../controllers/post.controller";
 import uploadFileMiddleware from "../middleware/multer.middleware";
 import { checkAuth } from "../middleware/checkAuth.middelware";
-import { getBookmarkedPosts } from "../controllers/bookmarks.controllers";
 
 const router = Router();
 
 router.post('/posts', checkAuth, uploadFileMiddleware, createPost);
 router.post('/reposts', checkAuth, uploadFileMiddleware, rePost);
 router.get('/posts', getAllPosts);
-router.get('/posts/bookmarks', checkAuth, getBookmarkedPosts);
 router.get('/posts/:id', getPostById);
 router.patch('/posts/:id', checkAuth, uploadFileMiddleware, updatePost);
 router.delete('/posts/:id', checkAuth, deletePost);
