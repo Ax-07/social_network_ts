da0819df54342a91754203d2050843ed2d82faed
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-14 22:08:09 +0200

    ajout des bookmarks pour les commentaires

--- server/src/controllers/bookmarks.controllers.ts
+++ server/src/controllers/bookmarks.controllers.ts
@@ -4,10 +4,10 @@ import { handleControllerError } from "../utils/errors/controllers.error";
 import { apiError, apiSuccess } from "../utils/functions/apiResponses";
 
 const addToBookmarks = async (req: Request, res: Response) => {
-  const { userId, postId } = req.body;
+  const { userId, postId, commentId } = req.body;
 
-  if (!userId || !postId) {
-    return apiError(res, "Missing fields", 400);
+  if (!userId || (!postId && !commentId) || (postId && commentId)) {
+    return apiError(res, "Invalid fields. You must provide either a postId or a commentId, but not both.", 400);
   }
 
   try {
@@ -16,44 +16,49 @@ const addToBookmarks = async (req: Request, res: Response) => {
       return apiError(res, "User not found", 404);
     }
 
-    const post = await db.Post.findByPk(postId);
-    if (!post) {
-      return apiError(res, "Post not found", 404);
+    if (postId) {
+      const post = await db.Post.findByPk(postId);
+      if (!post) {
+        return apiError(res, "Post not found", 404);
+      }
+    } else if (commentId) {
+      const comment = await db.Comment.findByPk(commentId);
+      if (!comment) {
+        return apiError(res, "Comment not found", 404);
+      }
     }
 
     // Vérifier si le post est déjà dans les bookmarks de l'utilisateur
     const bookmark = await db.UserBookmarks.findOne({
       where: {
-        userId: userId,
-        postId: postId,
+        userId,
+        postId: postId || null,
+        commentId: commentId || null,
       },
     });
 
     if (bookmark) {
-      // Si le post est déjà dans les bookmarks, le retirer
+      // Si le post/commentaire est déjà dans les bookmarks, le retirer
       await bookmark.destroy();
-      const updatedBookmarks  = await db.UserBookmarks.findAll({ where: { userId: userId }, attributes: ['postId'] });
-      return apiSuccess(res, "Post removed from bookmarks successfully", {bookmarks: updatedBookmarks}, 200);
+      const updatedBookmarks = await db.UserBookmarks.findAll({ where: { userId }, attributes: ['postId', 'commentId'] });
+      return apiSuccess(res, "Bookmark removed successfully", { bookmarks: updatedBookmarks }, 200);
     } else {
-      // Sinon, ajouter le post aux bookmarks
+      // Sinon, ajouter le post/commentaire aux bookmarks
       await db.UserBookmarks.create({
-        userId: userId,
-        postId: postId,
+        userId,
+        postId: postId || null,
+        commentId: commentId || null,
       });
-      const updatedBookmarks  = await db.UserBookmarks.findAll({ where: { userId: userId }, attributes: ['postId'] });
-      return apiSuccess(res, "Post added to bookmarks successfully", {bookmarks: updatedBookmarks}, 201);
+      const updatedBookmarks = await db.UserBookmarks.findAll({ where: { userId }, attributes: ['postId', 'commentId'] });
+      return apiSuccess(res, "Bookmark added successfully", { bookmarks: updatedBookmarks }, 201);
     }
   } catch (error) {
-    return handleControllerError(
-      res,
-      error,
-      "An error occurred while adding post to bookmarks"
-    );
+    return handleControllerError(res, error, "An error occurred while adding to bookmarks.");
   }
 };
 
-const getBookmarkedPosts = async (req: Request, res: Response) => {
-  const userId = req.query.id as string;
+const getBookmarks = async (req: Request, res: Response) => {
+  const userId = req.params.id as string;
   if (!userId) {
     return apiError(res, 'User ID is required', 400);
   }
@@ -66,18 +71,30 @@ const getBookmarkedPosts = async (req: Request, res: Response) => {
     }
 
     // Récupérer les posts bookmarkés par l'utilisateur via la table de jonction UserBookmarks
-    const bookmarkedPosts = await db.Post.findAll();
+    const allBookmarkedItem = await db.UserBookmarks.findAll({ 
+      where: { userId },
+      include: [
+        {
+          model: db.Post,
+          as: 'post',
+        },
+        {
+          model: db.Comment,
+          as: 'comment',
+        },
+      ],
+    });
 
     // Si aucun post n'est trouvé, retourner un tableau vide
-    if (bookmarkedPosts.length === 0) {
+    if (allBookmarkedItem.length === 0 ) {
       return apiSuccess(res, `No bookmarked posts found for user ${userId}`, [], 200);
     }
 
-    return apiSuccess(res, `Bookmarked posts for user ${userId}`, bookmarkedPosts, 200);
+    return apiSuccess(res, `Bookmarked posts for user ${userId}`, allBookmarkedItem, 200);
 
   } catch (error) {
     return handleControllerError(res, error, 'An error occurred while getting bookmarked posts.');
   }
 };
 
-export { addToBookmarks, getBookmarkedPosts };
+export { addToBookmarks, getBookmarks };
