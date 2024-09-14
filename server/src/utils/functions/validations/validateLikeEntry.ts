6ad8f26011fe8ed0de56e0161332d4c3a7e7dceb
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-14 22:22:48 +0200

    delete disliker et ajout commentId

--- server/src/utils/functions/validations/validateLikeEntry.ts
+++ server/src/utils/functions/validations/validateLikeEntry.ts
@@ -3,7 +3,8 @@ import { PostAttributes } from "../../../models/posts/post.model";
 import { UserAttributes } from "../../../models/users/user.model";
 
 interface LikeEntry extends Optional<PostAttributes & UserAttributes, "id"> {
-  postId: string;
+  postId?: string;
+  commentId?: string;
   likerId: string;
   dislikerId: string;
 }
@@ -15,12 +16,12 @@ const validateLikeEntry = (entry: LikeEntry) => {
     errors.push("LikerId must be a non-empty string");
   }
 
-  if (entry.dislikerId && (typeof entry.dislikerId !== "string" || entry.dislikerId.length === 0)) {
-    errors.push("DislikerId must be a non-empty string");
+  if (entry.postId && (typeof entry.postId !== "string" || entry.postId.length === 0)) {
+    errors.push("PostId must be a non-empty string");
   }
 
-  if (!entry.postId || typeof entry.postId !== "string" || entry.postId.length === 0) {
-    errors.push("PostId must be a non-empty string");
+  if (entry.commentId && (typeof entry.commentId !== "string" || entry.commentId.length === 0)) {
+    errors.push("CommentId must be a non-empty string");
   }
 
   return errors;
