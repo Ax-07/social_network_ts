d61d9108731d82297b3be2f4931fa830641299fc
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-18 18:43:02 +0200

    ajout messageList

--- client/src/layouts/sideColumn/SideColumn.tsx
+++ client/src/layouts/sideColumn/SideColumn.tsx
@@ -1,7 +1,10 @@
+import MessageList from "../../components/Display/conversation/messages/MessageList";
+
 const SideColumn = () => {
     return (
         <div className="side-column">
             <h2>SideColumn</h2>
+            <MessageList />
         </div>
     );
 };
