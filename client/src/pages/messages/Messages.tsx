860a007c71d6ffb94b8ca3b8688d647bf3e12a53
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-18 18:43:48 +0200

    ajout btnMessage et ConversationList

--- client/src/pages/messages/Messages.tsx
+++ client/src/pages/messages/Messages.tsx
@@ -1,7 +1,16 @@
+import BtnMessage from "../../components/Actions/btn-message/BtnMessage";
+import ConversationList from "../../components/Display/conversation/ConversationList";
+
 const Messages = () => {
     return (
-        <div>
-            <h1>Messages</h1>            
+        <div className="messages-page">
+            <header className="messages-page__header">
+                <h1 className="messages-page__title">Messages</h1>
+                <BtnMessage />
+            </header>
+            <div className="messages-page__content">
+                <ConversationList />
+            </div>
         </div>
     );
 };
