e78f1af32dac0a63bf76cbcb7627d9bae533351c
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-18 18:41:35 +0200

    ajout formOrigin et switchOrigin

--- client/src/components/Form/Form.tsx
+++ client/src/components/Form/Form.tsx
@@ -1,14 +1,14 @@
 import { useEffect, type FunctionComponent, type PropsWithChildren } from "react";
 import { ProfilPicture } from "../userProfile/UserProfileThumbnail";
 import PreviewPicture from "../Actions/addPicture/PreviewPicture";
-import { autoResizeTextarea } from "../../utils/functions/autoResizeTextarea";
 import Button from "../Base/button/Button";
 import InputPicture from "../Actions/addPicture/InputPicure";
 import RepostCard from "../Display/repost/RepostCard";
 import { useSelector } from "react-redux";
 import { RootState } from "../../services/stores";
-import { getTextareaCharacterCount } from "../../utils/functions/textareaCharacterCount";
 import { useForm } from "./hooks/useForm";
+import ContentField from "./inputFields/ContentField";
+import { FormOrigin, switchOrigin } from "./utils/switchOrigin";
 
 interface FormProps
   extends PropsWithChildren<{
@@ -18,19 +18,15 @@ interface FormProps
 
 const Form: FunctionComponent<FormProps> = ({ origin, handleSubmit }) => {
   const user = useSelector((state: RootState) => state.auth.user);
-  const { form, inputFileRef, setFormState, changeContent, changeFile, resetFile } = useForm(origin);
-  const { buttonText, placeholder } = switchOrigin(origin!);
+  const { form, inputFileRef, setFormState, changeFile, resetFile } = useForm(origin);
+  const { buttonText } = switchOrigin(origin!);
 
   useEffect(() => {
     // Reset le formulaire à chaque ouverture
-    setFormState({ userId: user?.id });
+    if (origin !== "modal-message") {
+      setFormState({ userId: user?.id });
+    }
   }, [user?.id]);
-
-  const handleInputTextarea = (e: React.FormEvent<HTMLTextAreaElement>) => {
-    const target = e.currentTarget;
-    autoResizeTextarea(target);
-    getTextareaCharacterCount(target);
-  };
 
   return (
     <div className={`addpost addpost--${origin}`}>
@@ -44,22 +40,7 @@ const Form: FunctionComponent<FormProps> = ({ origin, handleSubmit }) => {
             <option value="private">Privé</option>
           </select>
         </header>
-        <label htmlFor="content" className="sr-only">
-          Contenu de la publication
-        </label>
-        <textarea
-          className="addpost__input"
-          placeholder={placeholder}
-          name="content"
-          id="content"
-          rows={1}
-          onChange={(e) => changeContent(e.currentTarget.value)}
-          value={form.content}
-          onInput={handleInputTextarea}
-        />
-        <p id="content-description" className="sr-only">
-          {`Ajoutez du texte à votre publication. Limite de caractères : ??.`}
-        </p>
+        <ContentField origin={origin} />
         {form.isPreview && (
           <PreviewPicture
             media={form.preview as string}
@@ -93,89 +74,3 @@ const Form: FunctionComponent<FormProps> = ({ origin, handleSubmit }) => {
 };
 
 export default Form;
-
-/**
- * @description Type pour définir l'origine du formulaire de publication
- */
-export type FormOrigin =
-  | "modal-addPost" // Ajout de publication via le modal
-  | "modal-comment" // Ajout de commentaire via le modal
-  | "modal-comment-post" // Ajout de commentaire a un post via le boutton addComment de postCard
-  | "modal-comment-comment" // Ajout de commentaire a un commentaire via le boutton addComment de commentCard
-  | "page-home" // Ajout de publication via la page d'accueil
-  | "post-page-comment" // Ajout de commentaire via la page d'un post
-  | "comment-page-comment" // Ajout de commentaire via la page d'un commentaire
-  | "post-list" // Ajout de publication via la liste des publications ( a verifier l'utilité )
-  | "btn-repost" // Repost d'une publication
-  | "modal-repost" // Repost d'une publication via le modal
-  | "btn-repost-with-comment" // Repost d'une publication avec commentaire
-  | "modal-repost-comment" // Repost d'une publication avec commentaire via le modal
-  | "modal-message"; // Envoie de message via le modal
-
-/**
- * @description Fonction pour changer le texte du bouton et le placeholder du textarea, en fonction de l'origine du formulaire
- * @param origin
- * @returns buttonText et placeholder
- *
- */
-const switchOrigin = (origin: FormOrigin) => {
-  let buttonText = "";
-  let placeholder = "";
-
-  switch (origin) {
-    case "modal-addPost":
-      buttonText = "Poster";
-      placeholder = "Quoi de neuf ?";
-      break;
-    case "modal-comment":
-      buttonText = "Commenter";
-      placeholder = "Ajouter un commentaire...";
-      break;
-    case "modal-comment-post":
-      buttonText = "Commenter";
-      placeholder = "Ajouter un commentaire...";
-      break;
-    case "modal-comment-comment":
-      buttonText = "Commenter";
-      placeholder = "Ajouter un commentaire...";
-      break;
-    case "page-home":
-      buttonText = "Publier";
-      placeholder = "Quoi de neuf ?";
-      break;
-    case "post-page-comment":
-      buttonText = "Répondre";
-      placeholder = "Ajouter un commentaire...";
-      break;
-    case "comment-page-comment":
-      buttonText = "Répondre";
-      placeholder = "Ajouter un commentaire...";
-      break;
-    case "modal-repost":
-      buttonText = "Reposter";
-      placeholder = "Ajouter un commentaire...";
-      break;
-    case "btn-repost":
-      buttonText = "Reposter";
-      placeholder = "Ajouter un commentaire...";
-      break;
-    case "modal-repost-comment":
-      buttonText = "Reposter";
-      placeholder = "Ajouter un commentaire...";
-      break;
-    case "btn-repost-with-comment":
-      buttonText = "Reposter";
-      placeholder = "Ajouter un commentaire...";
-      break;
-    case "modal-message":
-      buttonText = "Envoyer";
-      placeholder = "Envoyer un message...";
-      break;
-    default:
-      buttonText = "Publier";
-      placeholder = "Quoi de neuf ?";
-      break;
-  }
-
-  return { buttonText, placeholder };
-};
