5dcbd7a855d178f8ae12f28899f791fcce789aa6
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-18 18:40:10 +0200

    creation composant ContentField

---
+++ client/src/components/Form/inputFields/ContentField.tsx
@@ -0,0 +1,43 @@
+import type { FunctionComponent } from "react";
+import { useForm } from "../hooks/useForm";
+import { autoResizeTextarea } from "../../../utils/functions/autoResizeTextarea";
+import { getTextareaCharacterCount } from "../../../utils/functions/textareaCharacterCount";
+import { FormOrigin, switchOrigin } from "../utils/switchOrigin";
+
+interface ContentFieldProps {
+  origin: FormOrigin;
+}
+
+const ContentField: FunctionComponent<ContentFieldProps> = ({ origin }) => {
+  const { form, changeContent } = useForm(origin);
+  const { placeholder } = switchOrigin(origin!);
+
+  const handleInputTextarea = (e: React.FormEvent<HTMLTextAreaElement>) => {
+    const target = e.currentTarget;
+    autoResizeTextarea(target);
+    getTextareaCharacterCount(target);
+  };
+  
+  return (
+    <>
+      <label htmlFor="content" className="sr-only">
+        Contenu de la publication
+      </label>
+      <textarea
+        className="addpost__input"
+        placeholder={placeholder}
+        name="content"
+        id="content"
+        rows={1}
+        onChange={(e) => changeContent(e.currentTarget.value)}
+        value={form.content}
+        onInput={handleInputTextarea}
+      />
+      <p id="content-description" className="sr-only">
+        {`Ajoutez du texte à votre publication. Limite de caractères : ??.`}
+      </p>
+    </>
+  );
+};
+
+export default ContentField;
