890931f43f0dd3ca4792a61cd42ef2dd1f06028b
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-23 22:38:09 +0200

    refacto

--- client/src/components/Form/hooks/useForm.ts
+++ client/src/components/Form/hooks/useForm.ts
@@ -5,46 +5,48 @@ import {
   resetForm,
   handleFileChange,
   handleContentChange,
-  setPreview,
-  setMimetype,
   FormState,
   selectFormByOrigin,
 } from "../../../services/forms/formSlice";
 import { useRef } from "react";
 import { FormOrigin } from "../utils/switchOrigin";
 
-export const useForm = (origin: FormOrigin) => {
+export const useForm = (origin?: FormOrigin) => {
   const dispatch = useDispatch();
-  // Vérifie que origin est défini avant de l'utiliser
-  if (!origin) {
-    console.error("Origin is not defined.");
-  }
-  // Sélectionner le formulaire spécifique à l'origin
-  const form = useSelector((state: RootState) => {
-    // Si origin est défini, on récupère le formulaire, sinon on retourne un objet vide
-    return origin ? selectFormByOrigin(state, origin) : {};
-  });
+
+  // Sélectionner le formulaire spécifique à l'origin uniquement si défini
+  const form = useSelector((state: RootState) => origin ? selectFormByOrigin(state, origin) : {});
+
   const inputFileRef = useRef<HTMLInputElement>(null);
 
   const setFormState = (formState: Partial<FormState>) => {
-    dispatch(setForm({ origin, formState }));
+    if (origin) {
+      dispatch(setForm({ origin, formState }));
+    }
   };
 
   const resetFormState = () => {
-    dispatch(resetForm({ origin }));
-    changeContent("");
-    resetFile();
+    if (origin) {
+      dispatch(resetForm({ origin }));
+    }
   };
 
   const changeFile = (file: File) => {
-    dispatch(handleFileChange({ origin, file }));
+    if (origin) {
+      dispatch(handleFileChange({ origin, file }));
 
-    const reader = new FileReader();
-    reader.onloadend = () => {
-      dispatch(setPreview({ origin, preview: reader.result as string }));
-      dispatch(setMimetype({ origin, mimetype: file.type }));
-    };
-    reader.readAsDataURL(file);
+      const reader = new FileReader();
+      reader.onloadend = () => {
+        dispatch(setForm({ 
+          origin, 
+          formState: { 
+            preview: reader.result as string,
+            mimetype: file.type
+          } 
+        }));
+      };
+      reader.readAsDataURL(file);
+    }
   };
 
   const embedYoutube = (url: string) => {
@@ -53,30 +55,49 @@ export const useForm = (origin: FormOrigin) => {
   };
 
   const changeContent = (content: string) => {
-    dispatch(handleContentChange({ origin, content }));
+    if (origin) {
+      dispatch(handleContentChange({ origin, content }));
 
-    if (content.includes("youtube.com") || content.includes("youtu.be")) {
-      const youtubeUrl = embedYoutube(content);
-      dispatch(setPreview({ origin, preview: youtubeUrl }));
-      dispatch(setMimetype({ origin, mimetype: "video/youtube" }));
-      dispatch(setForm({ origin, formState: { file: youtubeUrl } }));
-    } else if (
-      ((!content.includes("youtube.com") || !content.includes("youtu.be")) &&
-        form.file?.toString().includes("youtube.com")) ||
-      form.file?.toString().includes("youtu.be")
-    ) {
-      dispatch(setForm({ origin, formState: { file: "" } }));
-      dispatch(setPreview({ origin, preview: "" }));
-      dispatch(setMimetype({ origin, mimetype: "" }));
+      if (content.includes("youtube.com") || content.includes("youtu.be")) {
+        const youtubeUrl = embedYoutube(content);
+        dispatch(setForm({ 
+          origin, 
+          formState: { 
+            file: youtubeUrl,
+            preview: youtubeUrl,
+            isPreview: true,
+            mimetype: "video/youtube"
+          } 
+        }));
+
+      } else if (form.file?.toString().includes("youtube.com") || form.file?.toString().includes("youtu.be")) {
+        dispatch(setForm({ 
+          origin, 
+          formState: { 
+            file: "",
+            preview: "",
+            isPreview: false,
+            mimetype: ""
+          } 
+        }));
+      }
     }
   };
 
   const resetFile = () => {
-    dispatch(setForm({ origin, formState: { file: "", isPreview: false } }));
-    dispatch(setPreview({ origin, preview: "" }));
-    dispatch(setMimetype({ origin, mimetype: "" }));
-    if (inputFileRef.current) {
-      inputFileRef.current.value = "";
+    if (origin) {
+      dispatch(setForm({ 
+        origin, 
+        formState: { 
+          file: "", 
+          isPreview: false, 
+          preview: "", 
+          mimetype: "" 
+        } 
+      }));
+      if (inputFileRef.current) {
+        inputFileRef.current.value = "";
+      }
     }
   };
 
