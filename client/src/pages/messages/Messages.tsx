bbad7293a34e7edbbc5ce03abbbd2c86877d6267
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-25 22:01:24 +0200

    ajout responsive

--- client/src/pages/messages/Messages.tsx
+++ client/src/pages/messages/Messages.tsx
@@ -1,7 +1,11 @@
 import BtnMessage from "../../components/Actions/btn-message/BtnMessage";
 import ConversationList from "../../components/Display/conversation/ConversationList";
+import MessageList from "../../components/Display/conversation/messages/MessageList";
+import { useWindowSize } from "../../utils/hooks/useWindowSize";
 
 const Messages = () => {
+    const { windowWidth } = useWindowSize();
+    const isTablet = windowWidth <= 1020;
     return (
         <div className="messages-page">
             <header className="messages-page__header">
@@ -9,7 +13,7 @@ const Messages = () => {
                 <BtnMessage />
             </header>
             <div className="messages-page__content">
-                <ConversationList />
+                {!isTablet ? <ConversationList/> : <MessageList />}
             </div>
         </div>
     );
