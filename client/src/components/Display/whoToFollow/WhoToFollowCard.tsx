90d638b1e8cfab0a449fb2785401a8f7345785c9
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-26 21:28:21 +0200

    creation whotofollow card

--- client/src/components/Display/whoToFollow/WhoToFollowCard.tsx
+++ client/src/components/Display/whoToFollow/WhoToFollowCard.tsx
@@ -1,15 +1,32 @@
-import type { FunctionComponent } from 'react';
+import type { FunctionComponent } from "react";
+import MemoizedBtnFollow from "../../Actions/btn-follow/BtnFollow";
+import { useGetUserByIdQuery } from "../../../services/api/userApi";
+import {
+  UserNameHoverDisplayCard,
+  UserThumbnailHoverDisplayCard,
+} from "../../userProfile/UserHoverDisplayCard ";
 
-interface WhoToFollowCardProps {
-  name: string;
+interface UserToFollowCardProps {
+  userToFollow: {
+    id: string;
+    username: string;
+    profilPicture: string;
+  };
 }
 
-const WhoToFollowCard: FunctionComponent<WhoToFollowCardProps> = ({name}) => {
+const WhoToFollowCard: FunctionComponent<UserToFollowCardProps> = ({
+  userToFollow,
+}) => {
+  const { data: { data: user } = {} } = useGetUserByIdQuery(userToFollow.id);
   return (
-    <div>
-      <h2>{name}</h2>
-      
-    </div>
+    <article className="post-card userToFollow" aria-labelledby={`user-to-follow-${userToFollow.id}`}>
+      <UserThumbnailHoverDisplayCard user={user} />
+      <div className="userToFollow__info">
+        <UserNameHoverDisplayCard user={user} flexDirection="column"/>
+        <p className="fs-15-600">{user?.bio ?? "hello world"}</p>
+      </div>
+      <MemoizedBtnFollow userToFollowId={userToFollow.id} />
+    </article>
   );
 };
 
