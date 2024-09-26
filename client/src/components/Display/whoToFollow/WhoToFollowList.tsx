0c1f011a19bd224c06165b2e7cf96df8a968d870
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-09-26 21:28:35 +0200

    creation whotofollow liste

--- client/src/components/Display/whoToFollow/WhoToFollowList.tsx
+++ client/src/components/Display/whoToFollow/WhoToFollowList.tsx
@@ -1,9 +1,29 @@
+import { useSelector } from "react-redux";
+import { useGetWhoToFollowQuery } from "../../../services/api/followApi";
+import { RootState } from "../../../services/stores";
+import WhoToFollowCard from "./WhoToFollowCard";
+
 const WhoToFollowList = () => {
-    return (
-        <div>
-            
+  const userId = useSelector((state: RootState) => state?.auth?.user?.id);
+  const { data: { data: usersToFollow } = {} } = useGetWhoToFollowQuery(userId ?? "");
+  console.log("usersToFollow", usersToFollow);
+
+  return (
+    <>
+      {usersToFollow && usersToFollow.length > 0 && (
+        <div className="whoToFollow">
+          <h2 className="whoToFollow__title">À suivre</h2>
+          <ul className="whoToFollow__list">
+            {usersToFollow.map((userToFollow) => (
+              <li key={userToFollow.id} className="whoToFollow__item">
+                <WhoToFollowCard userToFollow={userToFollow} />
+              </li>
+            ))}
+          </ul>
         </div>
-    );
+      )}
+    </>
+  );
 };
 
 export default WhoToFollowList;
