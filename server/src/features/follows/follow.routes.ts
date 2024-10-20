e7b2e1349d9277d0c78e650bf983b7e87f3bf166
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-10-20 15:10:27 +0200

    ajout route whotofollow

--- server/src/features/follows/follow.routes.ts
+++ server/src/features/follows/follow.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { checkAuth } from '../../middleware/checkAuth.middelware';
-import { followUser, getFollowersNames } from './controllers/follow.controller';
+import { followUser, getFollowersNames, getWhoToFollow } from './controllers/follow.controller';
 
 const router = Router();
 
 router.patch('/follow', checkAuth, followUser);
 router.get('/users/:id/followers', checkAuth, getFollowersNames);
+router.get('/users/:id/whoToFollow', checkAuth, getWhoToFollow);
 
 export default router;
