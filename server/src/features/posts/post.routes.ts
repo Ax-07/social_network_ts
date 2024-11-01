080b2efdf33bbb93a3a5030d6387e86448222159
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-11-01 23:19:15 +0100

    ajout de nouvelles routes pour récupérer les posts par utilisateur, par abonnement et par hashtag, ainsi que les hashtags tendance

--- server/src/features/posts/post.routes.ts
+++ server/src/features/posts/post.routes.ts
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import { checkAuth } from "../../middleware/checkAuth.middelware";
 import uploadFileMiddleware from "../../middleware/multer.middleware";
-import { createPost, rePost, getAllPosts, getPostById, updatePost, deletePost, viewPost } from "./controllers/post.controller";
+import { createPost, rePost, getAllPosts, getPostById, updatePost, deletePost, viewPost, getPostsByUserId, getPostBySubscription, getPostsByHashtag, getTrendsHashtags } from "./controllers/post.controller";
 
 
 const router = Router();
@@ -10,7 +10,11 @@ router.post('/posts', checkAuth, uploadFileMiddleware, createPost);
 router.post('/reposts', checkAuth, uploadFileMiddleware, rePost);
 router.get('/posts', getAllPosts);
 router.get('/posts/:id', getPostById);
+router.get('/posts/user/:userId', checkAuth, getPostsByUserId);
+router.get('/posts/user/:userId/subscription', checkAuth, getPostBySubscription);
 router.patch('/posts/:id', checkAuth, uploadFileMiddleware, updatePost);
+router.get('/posts/hashtags/trends', getTrendsHashtags);
+router.get('/posts/hashtags/:hashtag', getPostsByHashtag);
 router.delete('/posts/:id', checkAuth, deletePost);
 router.post('/posts/views', checkAuth, viewPost);
 
