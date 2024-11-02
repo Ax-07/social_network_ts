57db5332d3c00147a1700650e78ac35e9e6952ea
Affringue xavier <99336880+Ax-07@users.noreply.github.com>
2024-11-02 22:22:45 +0100

    ajout des routes pour la récupération et la réinitialisation du mot de passe

--- server/src/features/auth/auth.routes.ts
+++ server/src/features/auth/auth.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from "express";
 import { googleAuthMiddleware } from "../../middleware/googleAuth.middleware";
-import { login, refreshToken, register } from "./controllers/auth.controller";
+import { forgotPassword, login, refreshToken, register, resetPassword } from "./controllers/auth.controller";
 import { googleLoginCallback, googleRefreshToken } from "./controllers/googleAuth.controller";
 
 
@@ -9,8 +9,11 @@ const router = Router();
 router.post('/register', register);
 router.post('/login', login);
 router.post('/refresh-token', refreshToken);
+router.post('/forgot-password', forgotPassword);
+router.post('/reset-password/:token', resetPassword);
 
 router.post('/google/callback', googleAuthMiddleware, googleLoginCallback);
 router.post('/google/refresh-token', googleAuthMiddleware, googleRefreshToken);
+
 
 export default router;
